6e64b9376f33e2f4d5a12edf1143c6c37a44d3b0
i1befree <i1befree@gmail.com>
2014-05-23 16:00:09 +0900

    Thrift prototyped version

---
+++ reef-inmemory/gen.sh
@@ -0,0 +1,3 @@
+thrift -out src/main/java --gen java:beans src/main/thrift/exceptions.thrift
+thrift -out src/main/java --gen java:beans src/main/thrift/entity.thrift
+thrift -out src/main/java --gen java:beans src/main/thrift/surf.thrift
