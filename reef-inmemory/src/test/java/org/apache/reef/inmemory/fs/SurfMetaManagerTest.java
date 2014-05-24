b0661b21baf1dd8be4fe8c52b8017ccca77589e9
i1befree <i1befree@gmail.com>
2014-05-24 13:41:04 +0900

    Reformat the code of testcase

--- reef-inmemory/src/test/java/org/apache/reef/inmemory/fs/SurfMetaManagerTest.java
+++ reef-inmemory/src/test/java/org/apache/reef/inmemory/fs/SurfMetaManagerTest.java
@@ -12,53 +12,53 @@ import java.util.List;
  * Test class for SurfMeta
  */
 public class SurfMetaManagerTest extends TestCase {
-    SurfMetaManager sm;
-    org.apache.reef.inmemory.fs.entity.User user;
+  SurfMetaManager sm;
+  org.apache.reef.inmemory.fs.entity.User user;
 
-    @Override
-    public void setUp(){
-        sm = new SurfMetaManager();
-        user = new org.apache.reef.inmemory.fs.entity.User();
-        user.setId("surf");
-        user.setGroup("surf");
+  @Override
+  public void setUp() {
+    sm = new SurfMetaManager();
+    user = new org.apache.reef.inmemory.fs.entity.User();
+    user.setId("surf");
+    user.setGroup("surf");
+  }
+
+  @Test
+  public void testMakeDirectory() throws FileAlreadyExistsException {
+    //Absolute path creating test
+    org.apache.reef.inmemory.fs.entity.FileMeta fm = sm.makeDirectory(new Path("/user/hive/home"), user);
+    assertEquals("Directory name is different.", fm.getFullPath(), "/user/hive/home");
+
+    try {
+      sm.makeDirectory(new Path("/user/hive"), user);
+      assertFalse("Directory /user/hive already exists", true);
+    } catch (FileAlreadyExistsException fe) {
+      assertTrue(true);
     }
 
-    @Test
-    public void testMakeDirectory() throws FileAlreadyExistsException {
-        //Absolute path creating test
-        org.apache.reef.inmemory.fs.entity.FileMeta fm = sm.makeDirectory(new Path("/user/hive/home"), user);
-        assertEquals("Directory name is different.", fm.getFullPath(), "/user/hive/home");
+    //Relative path creating test
+    fm = sm.makeDirectory(new Path("hive"), user);
+    assertEquals("Directory name is different.", "/user/surf/hive", fm.getFullPath());
+  }
 
-        try{
-            sm.makeDirectory(new Path("/user/hive"), user);
-            assertFalse("Directory /user/hive already exists", true);
-        }catch(FileAlreadyExistsException fe){
-            assertTrue(true);
-        }
+  @Test
+  public void testListStatus() throws FileNotFoundException, FileAlreadyExistsException {
+    List<org.apache.reef.inmemory.fs.entity.FileMeta> fms = sm.listStatus(new Path("/user/hive"), false, user);
+    assertEquals("Directory name is differenct", "/user/hive/home", fms.get(0).getFullPath());
 
-        //Relative path creating test
-        fm = sm.makeDirectory(new Path("hive"), user);
-        assertEquals("Directory name is different.", "/user/surf/hive", fm.getFullPath());
+    try {
+      fms = sm.listStatus(new Path("/user/test"), false, user);
+      assertFalse("Directory /user/hive already exists", true);
+    } catch (FileNotFoundException fe) {
+      assertTrue(true);
     }
 
-    @Test
-    public void testListStatus() throws FileNotFoundException, FileAlreadyExistsException {
-        List<org.apache.reef.inmemory.fs.entity.FileMeta> fms = sm.listStatus(new Path("/user/hive"), false, user);
-        assertEquals("Directory name is differenct", "/user/hive/home", fms.get(0).getFullPath());
+    sm.makeDirectory(new Path("test"), user);
 
-        try{
-            fms = sm.listStatus(new Path("/user/test"), false, user);
-            assertFalse("Directory /user/hive already exists", true);
-        }catch(FileNotFoundException fe){
-            assertTrue(true);
-        }
+    fms = sm.listStatus(new Path("./"), false, user);
+    assertEquals("Sub directories count is different", 2, fms.size());
 
-        sm.makeDirectory(new Path("test"), user);
-
-        fms = sm.listStatus(new Path("./"), false, user);
-        assertEquals("Sub directories count is different", 2, fms.size());
-
-        fms = sm.listStatus(new Path("/"), false, user);
-        assertEquals("Sub directories count is different", 2, fms.size());
-    }
+    fms = sm.listStatus(new Path("/"), false, user);
+    assertEquals("Sub directories count is different", 2, fms.size());
+  }
 }
--- reef-inmemory/src/test/java/org/apache/reef/inmemory/service/SurfMetaServiceImplTest.java
+++ reef-inmemory/src/test/java/org/apache/reef/inmemory/service/SurfMetaServiceImplTest.java
@@ -1,42 +1,32 @@
 package org.apache.reef.inmemory.service;
 
 import junit.framework.TestCase;
-import org.apache.reef.inmemory.fs.entity.FileMeta;
-import org.apache.reef.inmemory.fs.entity.User;
-import org.apache.reef.inmemory.fs.service.SurfMetaService;
-import org.apache.thrift.protocol.TCompactProtocol;
-import org.apache.thrift.protocol.TProtocol;
-import org.apache.thrift.transport.TFramedTransport;
-import org.apache.thrift.transport.TSocket;
-import org.apache.thrift.transport.TTransport;
 import org.apache.thrift.transport.TTransportException;
 import org.junit.Test;
-
-import java.util.List;
 
 /**
  *
  */
 public class SurfMetaServiceImplTest extends TestCase {
-    @Test
-    public void testMakeDirectory() throws TTransportException {
-        int port = 18000;
-/**
-        TNonblockingServerTransport serverTransport = new TNonblockingServerSocket(port, 30000);
-        SurfMetaService.Processor<SurfMetaService.Iface> processor = new SurfMetaService.Processor<SurfMetaService.Iface>(new SurfMetaServiceImpl());
+  @Test
+  public void testMakeDirectory() throws TTransportException {
+    int port = 18000;
+    /**
+     TNonblockingServerTransport serverTransport = new TNonblockingServerSocket(port, 30000);
+     SurfMetaService.Processor<SurfMetaService.Iface> processor = new SurfMetaService.Processor<SurfMetaService.Iface>(new SurfMetaServiceImpl());
 
-        TServer server = new THsHaServer(
-                new org.apache.thrift.server.THsHaServer.Args(
-                        serverTransport).processor(processor)
-                        .protocolFactory(new org.apache.thrift.protocol.TCompactProtocol.Factory())
-                        .workerThreads(10));
+     TServer server = new THsHaServer(
+     new org.apache.thrift.server.THsHaServer.Args(
+     serverTransport).processor(processor)
+     .protocolFactory(new org.apache.thrift.protocol.TCompactProtocol.Factory())
+     .workerThreads(10));
 
-        server.serve();
-**/
-    }
+     server.serve();
+     **/
+  }
 
-    public void testMakeDirectoryClient() throws Exception {
-        /*
+  public void testMakeDirectoryClient() throws Exception {
+  /*
         TTransport transport = null;
         transport = new TFramedTransport(new TSocket("localhost", 18000,
                 60000));
@@ -49,6 +39,6 @@ public class SurfMetaServiceImplTest extends TestCase {
         for(FileMeta fm : fms){
             System.out.println(fm.getFullPath());
         }
-        */
-    }
+  */
+  }
 }
