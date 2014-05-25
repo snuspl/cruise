162e786014f21fa0e74e9ff7fd8e8e3320a13fcc
Yunseong Lee <yunseong.lee@me.com>
2014-05-25 23:37:44 +0900

    Removed Auto-generated files and Updated pom.xml to handle them

--- reef-inmemory/gen.sh
+++
@@ -1,3 +0,0 @@
-thrift -out src/main/java --gen java:beans src/main/thrift/exceptions.thrift
-thrift -out src/main/java --gen java:beans src/main/thrift/entity.thrift
-thrift -out src/main/java --gen java:beans src/main/thrift/surf.thrift
