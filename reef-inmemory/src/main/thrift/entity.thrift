7a6fa99181bc292c8c16046ccb921bf0b187c222
i1befree <i1befree@gmail.com>
2014-05-30 16:58:26 +0900

    Comments in thrift file.

--- reef-inmemory/src/main/thrift/entity.thrift
+++ reef-inmemory/src/main/thrift/entity.thrift
@@ -1,26 +1,26 @@
 namespace java org.apache.reef.inmemory.fs.entity
 
 struct BlockInfo {
-	1: i64 blockId,
-	2: i32 offSet,
-	3: i32 length,
-	4: list<string> locations
+	1: i64 blockId, //Block unique id
+	2: i32 offSet, //order of block
+	3: i32 length, //the size of block by byte
+	4: list<string> locations //block location. Theses are server ip addresses containig the block.
 }
 
 struct User {
-	1: string id,
-	2: string group
+	1: string id, //userid
+	2: string group //usergroup
 }
 
 struct FileMeta{
-	1:i64 fileId,
-	2:string fileName,
-	3:string fullPath,
-	4:i64 fileSize,
-	5:i64 blockSize,
-	6:i64 creationTime,
-	7:bool directory,
-	8:list<BlockInfo> blocks,
-	9:bool complete,
-	10:User owner
+	1:i64 fileId, //File unique id
+	2:string fileName, //file name
+	3:string fullPath, //file's abolute path
+	4:i64 fileSize, //the size of file by byte
+	5:i64 blockSize, //the size of blocks consisting of file.
+	6:i64 creationTime, //file creation time
+	7:bool directory, // file or directory
+	8:list<BlockInfo> blocks, //Block informations consisting of file.
+	9:bool complete, //whether the file is complete or not
+	10:User owner //Owner information of thie file.
 }
--- reef-inmemory/src/main/thrift/surf.thrift
+++ reef-inmemory/src/main/thrift/surf.thrift
@@ -4,6 +4,8 @@ include "exceptions.thrift"
 namespace java org.apache.reef.inmemory.fs.service
 
 service SurfMetaService {
+  //list statuses of file or directories
 	list<entity.FileMeta> listStatus(1:string path, 2:bool recursive, 3:entity.User user) throws (1: exceptions.FileNotFoundException fe),
+	//make a new directory
 	entity.FileMeta makeDirectory(1:string path, 2:entity.User user) throws (1: exceptions.FileAlreadyExistsException fe)
 }
