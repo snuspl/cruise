6162b4b4c864bc1e68e85dc40fa6991a1df890fc
Yunseong Lee <yunseong.lee@me.com>
2014-05-30 22:42:40 +0900

    Correct typos and make the comments little bit prettier

--- reef-inmemory/src/main/thrift/entity.thrift
+++ reef-inmemory/src/main/thrift/entity.thrift
@@ -1,26 +1,26 @@
 namespace java org.apache.reef.inmemory.fs.entity
 
 struct BlockInfo {
-	1: i64 blockId, //Block unique id
-	2: i32 offSet, //order of block
-	3: i32 length, //the size of block by byte
-	4: list<string> locations //block location. Theses are server ip addresses containig the block.
+	1: i64 blockId,           // Block id (unique)
+	2: i32 offSet,            // Order of the block
+	3: i32 length,            // Size of the block in bytes
+	4: list<string> locations // Block location. These are server ip addresses containig the block.
 }
 
 struct User {
-	1: string id, //userid
-	2: string group //usergroup
+	1: string id,             // User id
+	2: string group           // User group
 }
 
 struct FileMeta{
-	1:i64 fileId, //File unique id
-	2:string fileName, //file name
-	3:string fullPath, //file's abolute path
-	4:i64 fileSize, //the size of file by byte
-	5:i64 blockSize, //the size of blocks consisting of file.
-	6:i64 creationTime, //file creation time
-	7:bool directory, // file or directory
-	8:list<BlockInfo> blocks, //Block informations consisting of file.
-	9:bool complete, //whether the file is complete or not
-	10:User owner //Owner information of thie file.
+	1:i64 fileId,             // File id (unique)
+	2:string fileName,        // File name
+	3:string fullPath,        // File's abolute path
+	4:i64 fileSize,           // Size of the file in bytes
+	5:i64 blockSize,          // Size of blocks consisting of the file.
+	6:i64 creationTime,       // File creation time
+	7:bool directory,         // Whether the file is a file or directory.
+	8:list<BlockInfo> blocks, // Information of blocks consisting of the file.
+	9:bool complete,          // Whether the file is complete or not
+	10:User owner             // Owner information of the file.
 }
