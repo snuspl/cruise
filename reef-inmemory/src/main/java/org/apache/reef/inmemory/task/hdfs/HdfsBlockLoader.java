59892bdcc3437cb115de946d96a808f68d6d78fc
Yunseong Lee <yunseong.lee@me.com>
2014-07-16 11:42:29 +0900

    Throw more specific Exceptions for each case

--- reef-inmemory/src/main/java/org/apache/reef/inmemory/task/hdfs/HdfsBlockLoader.java
+++ reef-inmemory/src/main/java/org/apache/reef/inmemory/task/hdfs/HdfsBlockLoader.java
@@ -10,6 +10,8 @@ import org.apache.hadoop.hdfs.server.common.HdfsServerConstants;
 import org.apache.hadoop.hdfs.server.datanode.CachingStrategy;
 import org.apache.hadoop.net.NetUtils;
 import org.apache.hadoop.security.token.Token;
+import org.apache.reef.inmemory.common.exceptions.ConnectionFailedException;
+import org.apache.reef.inmemory.common.exceptions.TransferFailedException;
 import org.apache.reef.inmemory.task.BlockId;
 import org.apache.reef.inmemory.task.BlockLoader;
 
@@ -37,68 +39,98 @@ public class HdfsBlockLoader implements BlockLoader {
   private final ExtendedBlock block;
   private final DatanodeID datanode;
   private final long blockSize;
-  private final Token<BlockTokenIdentifier> blockToken;
 
   /**
    * Constructor of BlockLoader
    */
   @Inject
   public HdfsBlockLoader(final HdfsBlockId id,
-                         final HdfsDatanodeInfo dnInfo) throws IOException {
+                         final HdfsDatanodeInfo dnInfo) {
     hdfsBlockId = id;
     block = new ExtendedBlock(id.getPoolId(), id.getBlockId(), id.getBlockSize(), id.getGenerationTimestamp());
     datanode = new DatanodeID(dnInfo.getIpAddr(), dnInfo.getHostName(), dnInfo.getStorageID(), dnInfo.getXferPort(), dnInfo.getInfoPort(), dnInfo.getInfoSecurePort(), dnInfo.getIpcPort());
     blockSize = id.getBlockSize();
-    Token<BlockTokenIdentifier> tempToken = null;
-    tempToken = new Token<>();
-    tempToken.decodeFromUrlString(id.getEncodedToken());
-    blockToken = tempToken;
   }
 
   /**
    * Loading a block from HDFS.
    * Too large block size(>2GB) is not supported.
    */
-  public byte[] loadBlock() throws IOException {
-    LOG.log(Level.INFO, "Start loading block {0} from datanode at {1}",
-            new String[]{Long.toString(hdfsBlockId.getBlockId()), datanode.getXferAddr()});
-    final byte[] buf = new byte[(int)blockSize];
+  public byte[] loadBlock() throws ConnectionFailedException, TokenDecodeFailedException, TransferFailedException {
+    final byte[] buf;
     final Configuration conf = new HdfsConfiguration();
 
-    // Allocate a ByteBuffer as a size of the Block
+    // Allocate a Byte array of the Block size.
     if(blockSize > Integer.MAX_VALUE)
       throw new UnsupportedOperationException("Currently we don't support large(>2GB) block");
+    buf = new byte[(int)blockSize];
+
+    LOG.log(Level.INFO, "Start loading block {0} from datanode at {1}",
+      new String[]{Long.toString(hdfsBlockId.getBlockId()), datanode.getXferAddr()});
 
     // Establish a connection against the DataNode
-    InetSocketAddress targetAddress = NetUtils.createSocketAddr(datanode.getXferAddr());
-    Socket s = NetUtils.getDefaultSocketFactory(conf).createSocket();
-    s.connect(targetAddress, HdfsServerConstants.READ_TIMEOUT);
-    s.setSoTimeout(HdfsServerConstants.READ_TIMEOUT);
+    InetSocketAddress targetAddress;
+    Socket socket;
+    try {
+      targetAddress = NetUtils.createSocketAddr(datanode.getXferAddr());
+      socket = NetUtils.getDefaultSocketFactory(conf).createSocket();
+      socket.connect(targetAddress, HdfsServerConstants.READ_TIMEOUT);
+      socket.setSoTimeout(HdfsServerConstants.READ_TIMEOUT);
+    } catch (IOException e) {
+      throw new ConnectionFailedException(e);
+    }
 
-    String fileName = targetAddress.toString() + ":" + block.getBlockId();
+    // Reproduce BlockToken from BlockId to read the block
+    final Token<BlockTokenIdentifier> blockToken;
+    try {
+      Token<BlockTokenIdentifier> tempToken;
+      tempToken = new Token<>();
+      tempToken.decodeFromUrlString(hdfsBlockId.getEncodedToken());
+      blockToken = tempToken;
+    } catch (IOException e) {
+      throw new TokenDecodeFailedException(e);
+    }
 
     // Set up BlockReader.
     // TODO Tweak config to improve the performance (e.g. utilize local short-circuit/task)
-    BlockReader blockReader = BlockReaderFactory.newBlockReader(
-      new DFSClient.Conf(conf), fileName, block,
-      blockToken, START_OFFSET, blockSize,
-      VERIFY_CHECKSUM, CLIENT_NAME, TcpPeerServer.peerFromSocket(s),
-      datanode, null, null, null, ALLOW_SHORT_CIRCUIT_LOCAL_READS,
-      CachingStrategy.newDefaultStrategy());
+    BlockReader blockReader;
+    String fileName = targetAddress.toString() + ":" + block.getBlockId();
+    try {
+      blockReader = BlockReaderFactory.newBlockReader(
+        new DFSClient.Conf(conf), fileName, block,
+        blockToken, START_OFFSET, blockSize,
+        VERIFY_CHECKSUM, CLIENT_NAME, TcpPeerServer.peerFromSocket(socket),
+        datanode, null, null, null, ALLOW_SHORT_CIRCUIT_LOCAL_READS,
+        CachingStrategy.newDefaultStrategy());
+    } catch (IOException e) {
+      throw new ConnectionFailedException(e);
+    }
 
     LOG.log(Level.INFO, "Data transfer loading block {0} from datanode at {1}",
-            new String[]{Long.toString(hdfsBlockId.getBlockId()), datanode.getXferAddr()});
+      new String[]{Long.toString(hdfsBlockId.getBlockId()), datanode.getXferAddr()});
+
     // Read the data using byte array buffer. BlockReader supports a method
     // to read the data into ByteBuffer directly, but it caused timeout.
-    int totalRead = 0;
-    do {
-      int nRead = blockReader.read(buf, totalRead, buf.length - totalRead);
-      totalRead += nRead;
-    } while(totalRead < blockSize);
-    blockReader.close();
+    try {
+      int totalRead = 0;
+      do {
+        int nRead = blockReader.read(buf, totalRead, buf.length - totalRead);
+        totalRead += nRead;
+      } while(totalRead < blockSize);
+    } catch (IOException e) {
+      throw new TransferFailedException(e);
+    }
+
+    // Close the BlockReader when done. It will be closed when the socket has closed.
+    try {
+      blockReader.close();
+    } catch (IOException e) {
+      LOG.log(Level.SEVERE, "Closing BlockReader for block {0} from datanode at {1} has failed",
+        new String[]{Long.toString(hdfsBlockId.getBlockId()), datanode.getXferAddr()});
+    }
 
     LOG.log(Level.INFO, "Done loading block {0} from datanode at {1}",
-            new String[]{Long.toString(hdfsBlockId.getBlockId()), datanode.getXferAddr()});
+      new String[]{Long.toString(hdfsBlockId.getBlockId()), datanode.getXferAddr()});
     return buf;
   }
   public BlockId getBlockId() {
