a1ccd5e9f9500cb7993fe28ee3efc207ff6222d3
Yunseong Lee <yunseong.lee@me.com>
2014-07-16 10:09:32 +0900

    Inherited IOException to handle it more robustly for each case

---
+++ reef-inmemory/src/main/java/org/apache/reef/inmemory/task/hdfs/TokenDecodeFailedException.java
@@ -0,0 +1,20 @@
+package org.apache.reef.inmemory.task.hdfs;
+
+import java.io.IOException;
+
+/**
+ * This exception is thrown when when it fails to decode the Token
+ */
+public class TokenDecodeFailedException extends IOException {
+  public TokenDecodeFailedException(String message) {
+    super(message);
+  }
+
+  public TokenDecodeFailedException(String message, Throwable cause) {
+    super(message, cause);
+  }
+
+  public TokenDecodeFailedException(Throwable cause) {
+    super(cause);
+  }
+}
