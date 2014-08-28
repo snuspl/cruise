67b304ab877a4fd76efa21b54127c4585cd13aac
Brian (Sunghoon) Cho <chobrian@gmail.com>
2014-08-28 10:19:00 +0900

    Add class comment

--- reef-inmemory/src/test/java/org/apache/reef/inmemory/common/IntensiveTests.java
+++ reef-inmemory/src/test/java/org/apache/reef/inmemory/common/IntensiveTests.java
@@ -1,4 +1,8 @@
 package org.apache.reef.inmemory.common;
 
+/**
+ * JUnit category for intensive (time-consuming) tests.
+ * Annotate using @Category on test classes or methods.
+ */
 public interface IntensiveTests {
 }
