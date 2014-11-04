fe6f6778da671cb0e7db7eb32edbb733850f5878
John Yang <johnyangk@gmail.com>
2014-11-04 15:16:33 +0900

    editing outputstream

--- reef-inmemory/src/main/java/org/apache/reef/inmemory/client/SurfFSOutputStream.java
+++ reef-inmemory/src/main/java/org/apache/reef/inmemory/client/SurfFSOutputStream.java
@@ -18,33 +18,23 @@ import java.util.Arrays;
 import java.util.Queue;
 import java.util.concurrent.*;
 
-/**
- * Output stream implementation. Register metadata when created,
- * and as Client writes the data allocate blocks and transfer the
- * data to the target block. When it fills the block, update metadata
- * and repeat this step until close is called.
- */
 public class SurfFSOutputStream extends OutputStream {
-  private final SurfMetaService.Client metaClient;
+  private final static int PACKET_SIZE = 512;
   private final Path path;
-
-  private String cachedBlockAddress;
   private final long blockSize;
-  private int blockOffset = 0;
 
   private final byte localBuf[] = new byte[PACKET_SIZE];
   private int localBufWriteCount = 0;
 
-  private final static int PACKET_SIZE = 512;
+  private AllocatedBlockInfo curAllocatedBlockInfo;
+  private long curBlockOffset = 0;
+
+  private final SurfMetaService.Client metaClient;
   private final Queue<Packet> packetQueue = new ConcurrentLinkedQueue<>();
   private final PacketStreamer streamer = new PacketStreamer();
   private final ExecutorService executor =
       new ThreadPoolExecutor(5, 5, 0L, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<Runnable>(80));
 
-  /**
-   * This constructor is called outside with the information to create a file
-   * @throws IOException If the file exists already
-   */
   public SurfFSOutputStream(Path path, SurfMetaService.Client metaClient, long blockSize) throws IOException, TException {
     this.blockSize = blockSize;
     this.metaClient = metaClient;
@@ -55,27 +45,22 @@ public class SurfFSOutputStream extends OutputStream {
   public void write(int b) throws IOException {
     localBuf[localBufWriteCount++] = (byte)b;
     if (localBufWriteCount == PACKET_SIZE) {
-      flush();
+      flushBuf(localBuf, 0, localBufWriteCount);
+      localBufWriteCount = 0;
     }
  }
 
   // blocking???
   @Override
   public void flush() throws IOException {
-    synchronized (this) {
-      // do metaClient.allocateBlock and handle boundary cases
-      if (localBufWriteCount != 0) {
-        if (blockOffset + localBufWriteCount <= blockSize)  {
-          sendPacket(cachedBlockAddress, blockOffset, Arrays.copyOfRange(localBuf, 0, localBufWriteCount));
-        } else {
-          final String nextAddress = metaClient.allocateBlock(path.toString(), blockSize);
-          sendPacket(cachedBlockAddress, blockOffset, Arrays.copyOfRange(localBuf, 0, blockSize-blockOffset));
-          sendPacket(nextAddress, 0, Arrays.copyOfRange(localBuf, localBuf.length+blockOffset-blockSize, localBuf.length));
-          cachedBlockAddress = nextAddress;
-        }
+    flushBuf(localBuf, 0, localBufWriteCount);
+    localBufWriteCount = 0;
 
-        blockOffset = (blockOffset + localBufWriteCount) % blockSize;
-        localBufWriteCount = 0;
+    while (packetQueue.size() > 0) {
+      try {
+        wait(1000);
+      } catch (InterruptedException e) {
+        // do not thrown an exception; just log.warning
       }
     }
   }
@@ -84,6 +69,7 @@ public class SurfFSOutputStream extends OutputStream {
   public void close() throws IOException {
     flush();
     this.executor.shutdown();
+    // metaClient.completeFile(String path, offset, blockSize, NodeInfo lastNode);
   }
 
   private class PacketStreamer implements Runnable {
@@ -91,33 +77,64 @@ public class SurfFSOutputStream extends OutputStream {
     public void run() {
       try {
         final Packet packet = packetQueue.remove();
-        final HostAndPort taskAddress = HostAndPort.fromString(packet.address);
-        final TTransport transport = new TFramedTransport(new TSocket(taskAddress.getHostText(), taskAddress.getPort()));
-        transport.open();
-        final TProtocol protocol = new TCompactProtocol(transport);
-        final SurfCacheService.Client cacheClient = new SurfCacheService.Client(protocol);
-
-        cacheClient.writeData(path, packet.offset, ByteBuffer.wrap(packet.buf));
+        final SurfCacheService.Client cacheClient = getCacheClient(packet.address);
+        cacheClient.writeData(path, packet.blockCount, blockSize, packet.offset, ByteBuffer.wrap(packet.buf));
       } catch (Exception e) {
         throw new RuntimeException("PacketStreamer exception");
       }
     }
   }
 
-  private class Packet {
-    final String address;
-    final long offset;
-    final byte[] buf;
+  /**
+   * @param b the buffer to flush
+   * @param start inclusive
+   * @param end exclusive
+   */
+  private void flushBuf(final byte[] b, final int start, final int end) {
+    // Need to allocate/initialize a new block
+    if (curBlockOffset == 0) {
+      curAllocatedBlockInfo = metaClient.allocateBlock(path.toString(), curBlockOffset, blockSize, myaddress);
+      final SurfCacheService.Client cacheClient = getCacheClient(curAllocatedBlockInfo);
+      // cacheClient.initBlock(path, offset, blockSize, AllocatedBlockInfo)
+    }
 
-    public Packet(final String address, final long offset, final byte[] buf) {
-      this.address = address;
-      this.offset = offset;
-      this.buf = buf;
+    if (curBlockOffset + (end - start) <= blockSize)  {
+      sendPacket(curAllocatedBlockInfo, curBlockOffset, Arrays.copyOfRange(b, start, end));
+      curBlockOffset = (curBlockOffset + (end - start)) % blockSize;
+    } else {
+      sendPacket(curAllocatedBlockInfo, curBlockOffset, Arrays.copyOfRange(b, start, end - (blockSize - curBlockOffset)));
+      curBlockOffset = (curBlockOffset + (end - start)) % blockSize;
+      flushBuf(Arrays.copyOfRange(end - (blockSize - curBlockOffset), end)); // flush the leftovers in b
     }
   }
 
-  private void sendPacket(final String address, final long offset, final byte[] buf) {
-    packetQueue.add(new Packet(address, offset, buf));
+  private void sendPacket(final AllocatedBlockInfo allocatedBlockInfo, final long offset, final byte[] buf) {
+    packetQueue.add(new Packet(address, blockCount, offset, buf));
     this.executor.submit(streamer);
+  }
+
+  private SurfCacheService.Client getCacheClient(final String address) {
+    try {
+      final HostAndPort taskAddress = HostAndPort.fromString(address);
+      final TTransport transport = new TFramedTransport(new TSocket(taskAddress.getHostText(), taskAddress.getPort()));
+      transport.open();
+      final TProtocol protocol = new TCompactProtocol(transport);
+      return new SurfCacheService.Client(protocol);
+    } catch (Exception e) {
+      throw new RuntimeException("getCacheClient exception");
+    }
+  }
+
+  private class Packet {
+    final AllocatedBlockInfo allocatedBlockInfo;
+    final long blockCount;
+    final long offset;
+    final byte[] buf;
+
+    public Packet(final AllocatedBlockInfo allocatedBlockInfo, final long blockCount, final long offset, final byte[] buf) {
+      this.blockCount = blockCount;
+      this.offset = offset;
+      this.buf = buf;
+    }
   }
 }
