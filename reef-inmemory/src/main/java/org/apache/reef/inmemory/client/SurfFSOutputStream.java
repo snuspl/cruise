997d59260d56f0b6916e1b6c762a99501d335907
John Yang <johnyangk@gmail.com>
2014-11-13 10:38:24 +0900

    add isLastPacket

--- reef-inmemory/src/main/java/org/apache/reef/inmemory/client/SurfFSOutputStream.java
+++ reef-inmemory/src/main/java/org/apache/reef/inmemory/client/SurfFSOutputStream.java
@@ -27,6 +27,7 @@ public class SurfFSOutputStream extends OutputStream {
 
   private byte localBuf[] = new byte[PACKET_SIZE];
   private int localBufWriteCount = 0;
+
   private AllocatedBlockInfo curAllocatedBlockInfo;
   private long curBlockOffset = -1;
   private long curBlockInnerOffset = 0;
@@ -54,7 +55,7 @@ public class SurfFSOutputStream extends OutputStream {
   public void write(int b) throws IOException {
     localBuf[localBufWriteCount++] = (byte)b;
     if (localBufWriteCount == PACKET_SIZE) {
-      flushLocalBuf();
+      flushLocalBuf(false);
     }
   }
 
@@ -80,7 +81,7 @@ public class SurfFSOutputStream extends OutputStream {
       startIndex += length;
 
       if (localBufWriteCount == PACKET_SIZE) {
-        flushLocalBuf();
+        flushLocalBuf(false);
       }
     }
   }
@@ -90,8 +91,28 @@ public class SurfFSOutputStream extends OutputStream {
    */
   @Override
   public void flush() throws IOException {
+    flush(false);
+  }
+
+  /**
+   * After flush(), notify SurfMetaServer of file completion
+   */
+  @Override
+  public void close() throws IOException {
+    flush(true);
+    this.executor.shutdown();
+    // TODO resolve the lastNode
+    NodeInfo lastNode = null;
+    try {
+      metaClient.completeFile(path, curBlockInnerOffset, blockSize, lastNode);
+    } catch (TException e) {
+      throw new IOException("Failed while closing the file", e);
+    }
+  }
+
+  public void flush(final boolean isLastPacket) throws IOException {
     if (localBufWriteCount > 0) {
-      flushLocalBuf();
+      flushLocalBuf(isLastPacket);
     }
 
     synchronized (this) {
@@ -105,24 +126,8 @@ public class SurfFSOutputStream extends OutputStream {
     }
   }
 
-  /**
-   * After flush(), notify SurfMetaServer of file completion
-   */
-  @Override
-  public void close() throws IOException {
-    flush();
-    this.executor.shutdown();
-    // TODO resolve the lastNode
-    NodeInfo lastNode = null;
-    try {
-      metaClient.completeFile(path, curBlockInnerOffset, blockSize, lastNode);
-    } catch (TException e) {
-      throw new IOException("Failed while closing the file", e);
-    }
-  }
-
-  private void flushLocalBuf() throws IOException {
-    flushBuf(localBuf, 0, localBufWriteCount);
+  private void flushLocalBuf(final boolean isLastPacket) throws IOException {
+    flushBuf(localBuf, 0, localBufWriteCount, isLastPacket);
     localBuf = new byte[PACKET_SIZE]; // create a new localBuf since the localBuf cannot be reused
     localBufWriteCount = 0;
   }
@@ -135,7 +140,7 @@ public class SurfFSOutputStream extends OutputStream {
    * @param start inclusive
    * @param end exclusive
    */
-  private void flushBuf(final byte[] b, final int start, final int end) throws IOException {
+  private void flushBuf(final byte[] b, final int start, final int end, final boolean isLastPacket) throws IOException {
     final int len = end - start;
 
     if (curBlockInnerOffset == 0) {
@@ -143,11 +148,11 @@ public class SurfFSOutputStream extends OutputStream {
     }
 
     if (curBlockInnerOffset + len <= blockSize)  {
-      sendPacket(ByteBuffer.wrap(b, start, len), len);
+      sendPacket(ByteBuffer.wrap(b, start, len), len, isLastPacket);
     } else {
       final int possibleLen = (int)(blockSize - curBlockInnerOffset); // this must be int because "possibleLen <= len"
-      sendPacket(ByteBuffer.wrap(b, start, possibleLen), possibleLen);
-      flushBuf(b, start+possibleLen, end); // Create another packet with the leftovers
+      sendPacket(ByteBuffer.wrap(b, start, possibleLen), possibleLen, false);
+      flushBuf(b, start+possibleLen, end, isLastPacket); // Create another packet with the leftovers
     }
   }
 
@@ -164,8 +169,8 @@ public class SurfFSOutputStream extends OutputStream {
     }
   }
 
-  private void sendPacket(final ByteBuffer buf, final long len) {
-    packetQueue.add(new Packet(curAllocatedBlockInfo, curBlockOffset, curBlockInnerOffset, buf, false)); // TODO: check for isLastPacket
+  private void sendPacket(final ByteBuffer buf, final long len, final boolean isLastPacket) {
+    packetQueue.add(new Packet(curAllocatedBlockInfo, curBlockOffset, curBlockInnerOffset, buf, isLastPacket));
     this.executor.submit(streamer);
     curBlockInnerOffset = (curBlockInnerOffset + len) % blockSize;
   }
