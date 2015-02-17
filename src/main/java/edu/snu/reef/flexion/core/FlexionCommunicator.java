b411376b89e63bb35d863f888c363314ae32add2
Kijung Shin <kijung.shin89@gmail.com>
2015-02-17 14:13:11 +0900

    generalize ComputeTask and ControllerTask

--- src/main/java/edu/snu/reef/flexion/core/FlexionCommunicator.java
+++
@@ -1,7 +0,0 @@
-package edu.snu.reef.flexion.core;
-
-public interface FlexionCommunicator {
-  public void send(Integer data) throws Exception;
-  public Integer receive() throws Exception;
-  public boolean terminate() throws Exception;
-}
