4230008ff53c4e01bb76e4235981a7ba70ec1f51
Jason (Joo Seong) Jeong <culex496782@gmail.com>
2015-06-22 01:59:11 +0900

    add comments to ContextMsgSender

--- src/main/java/edu/snu/reef/em/driver/ContextMsgSender.java
+++ src/main/java/edu/snu/reef/em/driver/ContextMsgSender.java
@@ -6,7 +6,6 @@ import org.apache.reef.driver.parameters.ContextActiveHandlers;
 import org.apache.reef.driver.parameters.TaskRunningHandlers;
 import org.apache.reef.driver.task.RunningTask;
 import org.apache.reef.tang.Configuration;
-import org.apache.reef.tang.JavaConfigurationBuilder;
 import org.apache.reef.tang.Tang;
 import org.apache.reef.tang.annotations.Unit;
 import org.apache.reef.wake.EventHandler;
@@ -15,16 +14,31 @@ import javax.inject.Inject;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Class for sending messages to evaluator Contexts using the Driver's remote manager
+ */
 @Unit
 @DriverSide
 public final class ContextMsgSender {
+
+  /**
+   * Map of Context/Task ids to the corresponding ActiveContext object
+   */
   private Map<String, ActiveContext> contextMap;
 
   @Inject
-  ContextMsgSender() {
+  private ContextMsgSender() {
     contextMap = new HashMap<>();
   }
 
+  /**
+   * Send a byte array message to the specified Context.
+   * If the string id indicates a Task, then the message is sent to the Context
+   * that is directly beneath that Task
+   *
+   * @param id Context/Task string identifier
+   * @param msg byte array message to send
+   */
   public void send(final String id, final byte[] msg) {
     final ActiveContext activeContext = contextMap.get(id);
     if (activeContext == null) {
