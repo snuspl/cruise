370c2bc503577abadbd25bce4b8691ff35fa7479
Jason (Joo Seong) Jeong <culex496782@gmail.com>
2015-06-22 18:37:45 +0900

    add avro

---
+++ src/main/java/edu/snu/reef/em/msg/ElasticMemoryMessageCodec.java
@@ -0,0 +1,6 @@
+package edu.snu.reef.em.msg;
+
+import org.apache.reef.wake.remote.Codec;
+
+public class ElasticMemoryMessageCodec implements Codec<edu.snu.reef.em.avro.AvroElasticMemoryMessage> {
+}
