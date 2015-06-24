62df32edd868efdb074f830d3c87c7286fab4d85
Jason (Joo Seong) Jeong <culex496782@gmail.com>
2015-06-24 17:33:37 +0900

    bind context ids to NetworkService

--- src/main/java/edu/snu/reef/em/driver/ElasticMemoryEvaluatorConfiguration.java
+++ src/main/java/edu/snu/reef/em/driver/ElasticMemoryEvaluatorConfiguration.java
@@ -6,37 +6,57 @@ import edu.snu.reef.em.ns.*;
 import edu.snu.reef.em.task.ElasticMemoryClient;
 import edu.snu.reef.em.task.ElasticMemoryStoreClient;
 import edu.snu.reef.em.task.MemoryStoreClient;
+import edu.snu.reef.em.task.NSWrapperToContext;
+import org.apache.reef.annotations.audience.DriverSide;
 import org.apache.reef.driver.context.ServiceConfiguration;
 import org.apache.reef.evaluator.context.parameters.ContextMessageHandlers;
+import org.apache.reef.evaluator.context.parameters.ContextStartHandlers;
+import org.apache.reef.evaluator.context.parameters.ContextStopHandlers;
+import org.apache.reef.io.network.naming.NameServer;
+import org.apache.reef.io.network.util.StringIdentifierFactory;
 import org.apache.reef.tang.Configuration;
 import org.apache.reef.tang.Configurations;
 import org.apache.reef.tang.Tang;
+import org.apache.reef.wake.IdentifierFactory;
 
 import javax.inject.Inject;
+import java.util.HashSet;
+import java.util.Set;
 
+@DriverSide
 public class ElasticMemoryEvaluatorConfiguration {
 
   final NSWrapperDriver nsWrapperDriver;
+  final NameServer nameServer;
+  final IdentifierFactory ifac = new StringIdentifierFactory();
+  final Set<String> contextIdSet = new HashSet<>();
 
   @Inject
-  private ElasticMemoryEvaluatorConfiguration(final NSWrapperDriver nsWrapperDriver) {
+  private ElasticMemoryEvaluatorConfiguration(final NSWrapperDriver nsWrapperDriver,
+                                              final NameServer nameServer) {
     this.nsWrapperDriver = nsWrapperDriver;
+    this.nameServer = nameServer;
   }
 
   public Configuration getContextConfiguration() {
     return Tang.Factory.getTang().newConfigurationBuilder()
         .bindSetEntry(ContextMessageHandlers.class, ElasticMemoryCtrlMsgHandler.class)
+        .bindSetEntry(ContextStartHandlers.class, ElasticMemoryClient.class)
+        .bindSetEntry(ContextStartHandlers.class, NSWrapperToContext.BindNSWrapperToContext.class)
+        .bindSetEntry(ContextStopHandlers.class, NSWrapperToContext.UnbindNSWrapperToContext.class)
         .build();
   }
 
   public Configuration getServiceConfiguration() {
+
     final Configuration nsWrapperConf =
         nsWrapperDriver.getConfiguration(ElasticMemoryMessageCodec.class,
                                          ElasticMemoryMessageHandlerWrapperImpl.class);
 
     final Configuration serviceConf = ServiceConfiguration.CONF
         .set(ServiceConfiguration.SERVICES, ElasticMemoryStoreClient.class)
-        .set(ServiceConfiguration.ON_CONTEXT_STARTED, ElasticMemoryClient.class)
+//        .set(ServiceConfiguration.ON_CONTEXT_STARTED, ElasticMemoryClient.class)
+//        .set(ServiceConfiguration.ON_CONTEXT_STOP, UnBindNSWrapperToContext.class)
         .build();
 
     final Configuration bindConf = Tang.Factory.getTang().newConfigurationBuilder()
@@ -45,6 +65,4 @@ public class ElasticMemoryEvaluatorConfiguration {
 
     return Configurations.merge(nsWrapperConf, serviceConf, bindConf);
   }
-
-
 }
---
+++ src/main/java/edu/snu/reef/em/task/NSWrapperToContext.java
@@ -0,0 +1,38 @@
+package edu.snu.reef.em.task;
+
+import edu.snu.reef.em.ns.NSWrapperClient;
+import org.apache.reef.evaluator.context.events.ContextStart;
+import org.apache.reef.evaluator.context.events.ContextStop;
+import org.apache.reef.io.network.impl.NetworkService;
+import org.apache.reef.tang.annotations.Unit;
+import org.apache.reef.wake.EventHandler;
+import org.apache.reef.wake.IdentifierFactory;
+
+import javax.inject.Inject;
+
+@Unit
+public final class NSWrapperToContext {
+
+  private NetworkService networkService;
+  private IdentifierFactory ifac;
+
+  @Inject
+  private NSWrapperToContext(NSWrapperClient nsWrapperClient) {
+    this.networkService = nsWrapperClient.getNetworkService();
+    this.ifac = this.networkService.getIdentifierFactory();
+  }
+
+  public final class BindNSWrapperToContext implements EventHandler<ContextStart> {
+    @Override
+    public void onNext(final ContextStart contextStart) {
+      networkService.registerId(ifac.getNewInstance(contextStart.getId()));
+    }
+  }
+
+  public final class UnbindNSWrapperToContext implements EventHandler<ContextStop> {
+    @Override
+    public void onNext(final ContextStop contextStop) {
+      networkService.unregisterId(ifac.getNewInstance(contextStop.getId()));
+    }
+  }
+}
