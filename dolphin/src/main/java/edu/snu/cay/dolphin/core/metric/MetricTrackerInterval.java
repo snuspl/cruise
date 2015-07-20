abc8684d65f96e60851e4044f8341a9771ffc7e4
Yunseong Lee <yunseong@apache.org>
2015-07-20 22:10:30 +0900

    Change the synchronization methods

--- dolphin/src/main/java/edu/snu/cay/dolphin/core/metric/MetricTrackerInterval.java
+++ dolphin/src/main/java/edu/snu/cay/dolphin/core/metric/MetricTrackerInterval.java
@@ -18,12 +18,13 @@ package edu.snu.cay.dolphin.core.metric;
 import javax.inject.Inject;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Metric tracker for intervals, which are generated from start() to stop().
  * The beginning and end of each interval are specified via beginInterval(key)
  * and endInterval(key).
+ *
+ * This class is thread-safe because multiple threads can access it simultaneously.
  *
  * MetricException is thrown when the beginInterval() is called more than once
  * for the same key, or endInterval() is called for the key that does not exist.
@@ -49,29 +50,34 @@ public class MetricTrackerInterval implements MetricTracker {
 
   /**
    * The moments when startInterval() are called.
-   * This should be thread-safe because multiple threads can access it simultaneously.
    */
-  private final Map<String, Double> beginTimes = new ConcurrentHashMap<>();
+  private final Map<String, Double> beginTimes = new HashMap<>();
 
   /**
    * The intervals that endInterval() are called.
-   * This should be thread-safe because multiple threads can access it simultaneously.
    */
-  private final Map<String, Double> intervals = new ConcurrentHashMap<>();
+  private final Map<String, Double> intervals = new HashMap<>();
+
+  /**
+   * Indicates that the intervals are ready to record.
+   */
+  private boolean isStarted = false;
 
   @Inject
   private MetricTrackerInterval() {
   }
 
   @Override
-  public void start() {
+  public synchronized void start() {
+    isStarted = true;
   }
 
   @Override
-  public Map<String, Double> stop() {
+  public synchronized Map<String, Double> stop() {
     final Map<String, Double> result = new HashMap<>();
     result.putAll(intervals);
 
+    isStarted = false;
     beginTimes.clear();
     intervals.clear();
 
@@ -86,9 +92,14 @@ public class MetricTrackerInterval implements MetricTracker {
   /**
    * Mark the beginning of interval.
    * @param key identifier to distinguish the interval
-   * @throws MetricException if beginInterval() was already called on the same key.
+   * @throws MetricException if the metricTracker is not between start() and stop(),
+   * or beginInterval() was already called on the same key.
    */
-  public void beginInterval(final String key) throws MetricException {
+  public synchronized void beginInterval(final String key) throws MetricException {
+    if (!isStarted) {
+      throw new MetricException("MetricTracker is not started");
+    }
+
     if (beginTimes.containsKey(key)) {
       throw new MetricException("beginInterval() was already called on " + key);
     } else {
@@ -100,10 +111,15 @@ public class MetricTrackerInterval implements MetricTracker {
   /**
    * Mark the end of interval.
    * @param key identifier to distinguish the interval
-   * @throws MetricException if endInterval() was already called on the same key,
-   *         or beginInterval() was not called on the key
+   * @throws MetricException if the metricTracker is not between start() and stop(),
+   * endInterval() was already called on the same key,
+   * or beginInterval() was not called on the key
    */
-  public void endInterval(final String key) throws MetricException {
+  public synchronized void endInterval(final String key) throws MetricException {
+    if (!isStarted) {
+      throw new MetricException("MetricTracker is not started");
+    }
+
     if (intervals.containsKey(key)) {
       throw new MetricException("endInterval() was already called on " + key);
     }
