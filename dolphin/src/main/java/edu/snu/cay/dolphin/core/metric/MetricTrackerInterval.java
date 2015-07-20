ddc61443c4ede6fad9e7acee15914800d2e3669f
Yunseong Lee <yunseong@apache.org>
2015-07-20 18:09:19 +0900

    Rename the UserMetricTracker to MetricTrackerInterval

---
+++ dolphin/src/main/java/edu/snu/cay/dolphin/core/metric/MetricTrackerInterval.java
@@ -0,0 +1,97 @@
+/**
+ * Copyright (C) 2015 Seoul National University
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package edu.snu.cay.dolphin.core.metric;
+
+import javax.inject.Inject;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+/**
+ * Metric tracker for intervals, which are generated from start() to stop().
+ * The beginning and end of each interval are specified via beginInterval(key)
+ * and endInterval(key).
+ *
+ * MetricException is thrown when the beginInterval() is called more than once
+ * for the same key, or endInterval() is called for the key that does not exist.
+ *
+ * Note the intervals that do not call endInterval() are ignored.
+ */
+public class MetricTrackerInterval implements MetricTracker {
+
+  /**
+   * The moments when startInterval() are called.
+   */
+  private final Map<String, Double> beginTimes = new ConcurrentHashMap<>();
+
+  /**
+   * The intervals that endInterval() are called.
+   */
+  private final Map<String, Double> intervals = new ConcurrentHashMap<>();
+
+  @Inject
+  public MetricTrackerInterval() {
+  }
+
+  @Override
+  public void start() {
+    beginTimes.clear();
+    intervals.clear();
+  }
+
+  @Override
+  public Map<String, Double> stop() {
+    return intervals;
+  }
+
+  @Override
+  public void close() throws Exception {
+  }
+
+  /**
+   * Mark the beginning of interval.
+   * @param key identifier to distinguish the interval
+   * @throws MetricException if beginInterval() was already called on the same key.
+   */
+  public void beginInterval(final String key) throws MetricException {
+    if (beginTimes.containsKey(key)) {
+      throw new MetricException("beginInterval() was already called on " + key);
+    } else {
+      final long beginTime = System.currentTimeMillis();
+      beginTimes.put(key, (double) beginTime);
+    }
+  }
+
+  /**
+   * Mark the end of interval.
+   * @param key identifier to distinguish the interval
+   * @throws MetricException if endInterval() was already called on the same key,
+   *         or beginInterval() was not called on the key
+   */
+  public void endInterval(final String key) throws MetricException {
+    if (intervals.containsKey(key)) {
+      throw new MetricException("endInterval() was already called on " + key);
+    }
+
+    if (beginTimes.containsKey(key)) {
+      final long endTime = System.currentTimeMillis();
+      final double beginTime = beginTimes.get(key);
+      intervals.put(key, (double) endTime - beginTime);
+    } else {
+      throw new MetricException("beginInterval() was not called on " + key);
+    }
+  }
+}
