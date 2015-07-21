0862e8ea54cf6dcd947cf65903699e00dfee96a7
Yunseong Lee <yunseong@apache.org>
2015-07-21 14:54:38 +0900

    Update comments in the MainCtrlTask of clustering algorithms

--- dolphin/src/main/java/edu/snu/cay/dolphin/examples/ml/algorithms/clustering/em/EMMainCtrlTask.java
+++ dolphin/src/main/java/edu/snu/cay/dolphin/examples/ml/algorithms/clustering/em/EMMainCtrlTask.java
@@ -113,7 +113,8 @@ public final class EMMainCtrlTask extends UserControllerTask
   }
 
   /**
-   * Receive initial centroids from the preprocess task
+   * Receive the initial centroids from the preprocess task.
+   * {@link edu.snu.cay.dolphin.examples.ml.algorithms.clustering.ClusteringPreCtrlTask}
    */
   @Override
   public void initialize() {
--- dolphin/src/main/java/edu/snu/cay/dolphin/examples/ml/algorithms/clustering/kmeans/KMeansMainCtrlTask.java
+++ dolphin/src/main/java/edu/snu/cay/dolphin/examples/ml/algorithms/clustering/kmeans/KMeansMainCtrlTask.java
@@ -95,7 +95,8 @@ public final class KMeansMainCtrlTask extends UserControllerTask
   }
 
   /**
-   * Receive initial centroids from the preprocess task
+   * Receive the initial centroids from the preprocess task.
+   * {@link edu.snu.cay.dolphin.examples.ml.algorithms.clustering.ClusteringPreCtrlTask}
    */
   @Override
   public void initialize() {
