13f2d15fbe03268f89c4a95468a874a0b8c67558
Brian (Sunghoon) Cho <chobrian@gmail.com>
2015-07-31 14:11:12 +0900

    Resolve checkstyle, license issues with NetworkConnectionService port.

--- services/elastic-memory/src/main/java/edu/snu/cay/services/em/ns/parameters/package-info.java
+++ services/elastic-memory/src/main/java/edu/snu/cay/services/em/ns/parameters/package-info.java
@@ -13,12 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+/**
+ * NetworkConnectionService related parameter classes.
+ */
 package edu.snu.cay.services.em.ns.parameters;
-
-import org.apache.reef.tang.annotations.Name;
-import org.apache.reef.tang.annotations.NamedParameter;
-import org.apache.reef.wake.remote.Codec;
-
-@NamedParameter(doc = "Network connection service codec for EM")
-public final class EMCodec implements Name<Codec<?>> {
-}
