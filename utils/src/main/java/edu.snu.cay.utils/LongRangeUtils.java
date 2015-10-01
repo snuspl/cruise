b657b5a1fa5df52ea0805a7b1a70c72b77944c35
Yunseong Lee <yunseong@apache.org>
2015-10-01 16:24:29 +0800

    Add utility methods for LongRange and add tests

--- utils/src/main/java/edu.snu.cay.utils/LongRangeUtils.java
+++ utils/src/main/java/edu.snu.cay.utils/LongRangeUtils.java
@@ -17,9 +17,11 @@ package edu.snu.cay.utils;
 
 import org.apache.commons.lang.math.LongRange;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.SortedSet;
+import java.util.TreeSet;
 
 /**
  * Utilities for dealing with {@code LongRange}s.
@@ -30,6 +32,32 @@ public final class LongRangeUtils {
    * Should not be instantiated.
    */
   private LongRangeUtils() {
+  }
+
+  /**
+   * Create an empty set to put LongRanges.
+   * The ranges are ordered in the set based on the following rule:
+   *   1) If max(range1) < min(range2), then range1 < range2 because all the values
+   *   in range1 is smaller than the numbers in range2.
+   *   2) If min(range1) > max(range2), then range1 > range2.
+   *   3) Otherwise, the order between two ranges are not clear. So users need more caution
+   *   when handling this case.
+   * @return An empty set.
+   */
+  public static TreeSet<LongRange> createEmptyTreeSet() {
+    final Comparator<LongRange> longRangeComparator = new Comparator<LongRange>() {
+      @Override
+      public int compare(final LongRange o1, final LongRange o2) {
+        if (o1.getMaximumLong() < o2.getMinimumLong()) {
+          return -1;
+        } else if (o1.getMinimumLong() > o2.getMaximumLong()) {
+          return 1;
+        } else {
+          return 0;
+        }
+      }
+    };
+    return new TreeSet<>(longRangeComparator);
   }
 
   /**
@@ -75,5 +103,64 @@ public final class LongRangeUtils {
       numUnits += (idRange.getMaximumLong() - idRange.getMinimumLong() + 1);
     }
     return numUnits;
+  }
+
+  /**
+   * @return Intersection of the two ranges. {@code null} if the ranges are disjoint.
+   */
+  public static LongRange getIntersection(final LongRange range1, final LongRange range2) {
+    if (!range1.overlapsRange(range2)) {
+      return null;
+    } else {
+      // The intersection is either [larger min, smaller max] or [smaller max, larger min]
+      final long min1 = range1.getMinimumLong();
+      final long max1 = range1.getMaximumLong();
+      final long min2 = range2.getMinimumLong();
+      final long max2 = range2.getMaximumLong();
+
+      // LongRange does not care about the order.
+      return new LongRange(Math.min(max1, max2), Math.max(min1, min2));
+    }
+  }
+
+  /**
+   * @return Complement of the two ranges (range1 - range2). If the range2 is inside range1, then
+   * returns the two sub-ranges which is split into two.
+   */
+  public static TreeSet<LongRange> getComplement(final LongRange range1, final LongRange range2) {
+    final TreeSet<LongRange> complement = LongRangeUtils.createEmptyTreeSet();
+    if (!range1.overlapsRange(range2)) {
+      complement.add(range1);
+      return complement;
+    } else {
+      final LongRange intersect = getIntersection(range1, range2);
+      // If two sections are overlapping, we can divide into three parts: LEFT | CENTER | RIGHT
+      // LEFT ([leftEnd (centerLeft-1)] and RIGHT ([(centerRight+1) rightEnd]) if they are not empty.
+      final long min1 = range1.getMinimumLong();
+      final long max1 = range1.getMaximumLong();
+      final long min2 = intersect.getMinimumLong();
+      final long max2 = intersect.getMaximumLong();
+      final long minMax = Math.min(max1, max2);
+      final long maxMin = Math.max(min1, min2);
+
+      final long leftEnd = Math.min(min1, min2);
+      final long centerLeft = Math.min(minMax, maxMin);
+      final long centerRight = Math.max(minMax, maxMin);
+      final long rightEnd = Math.max(max1, max2);
+
+      // Add LEFT only if exists.
+      if (leftEnd < centerLeft) {
+        final LongRange left = new LongRange(leftEnd, centerLeft - 1);
+        complement.add(left);
+      }
+
+      // Add RIGHT only if exists.
+      if (rightEnd > centerRight) {
+        final LongRange right = new LongRange(centerRight + 1, rightEnd);
+        complement.add(right);
+      }
+
+      return complement;
+    }
   }
 }
