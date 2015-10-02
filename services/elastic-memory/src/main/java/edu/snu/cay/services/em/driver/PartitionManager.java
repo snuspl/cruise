ffe28f792c9409abb2edaa54d5cb69fbbb077e88
Gyeongin Yu <gyeonginyu@gmail.com>
2015-10-02 16:33:35 +0900

    Add getTypes api to PartitionManager

--- services/elastic-memory/src/main/java/edu/snu/cay/services/em/driver/PartitionManager.java
+++ services/elastic-memory/src/main/java/edu/snu/cay/services/em/driver/PartitionManager.java
@@ -168,6 +168,18 @@ public final class PartitionManager {
   }
 
   /**
+   * Get the set of existing data types in a evaluator.
+   * @return Set of data types. An empty set is returned if there is no data types in the evaluator.
+   */
+  public synchronized Set<String> getDataTypes(final String evalId) {
+    if (!evalPartitions.containsKey(evalId)) {
+      LOG.log(Level.WARNING, "The evaluator {0} does not exist.", evalId);
+      return new HashSet<>();
+    }
+    return new HashSet<>(evalPartitions.get(evalId).keySet());
+  }
+
+  /**
    * Get the existing range set in a evaluator of a type.
    * @return Sorted set of ranges. An empty set is returned if there is no matched range.
    */
