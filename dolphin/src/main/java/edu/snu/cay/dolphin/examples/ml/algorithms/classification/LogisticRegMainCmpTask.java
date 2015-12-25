75a8f5e46d71509f217f94abfacb42f347edad4c
WooYeon <wylee.xyzi@gmail.com>
2015-12-26 02:27:23 +0900

    Make LR's mainCmpTask use WorkloadQuota

--- dolphin/src/main/java/edu/snu/cay/dolphin/examples/ml/algorithms/classification/LogisticRegMainCmpTask.java
+++ dolphin/src/main/java/edu/snu/cay/dolphin/examples/ml/algorithms/classification/LogisticRegMainCmpTask.java
@@ -16,6 +16,7 @@
 package edu.snu.cay.dolphin.examples.ml.algorithms.classification;
 
 import edu.snu.cay.dolphin.core.UserComputeTask;
+import edu.snu.cay.dolphin.core.WorkloadQuota;
 import edu.snu.cay.dolphin.examples.ml.data.LinearModel;
 import edu.snu.cay.dolphin.examples.ml.data.LogisticRegSummary;
 import edu.snu.cay.dolphin.examples.ml.data.Row;
@@ -26,20 +27,26 @@ import edu.snu.cay.dolphin.examples.ml.regularization.Regularization;
 import edu.snu.cay.dolphin.groupcomm.interfaces.DataBroadcastReceiver;
 import edu.snu.cay.dolphin.groupcomm.interfaces.DataReduceSender;
 import edu.snu.cay.services.em.evaluator.api.MemoryStore;
+import org.apache.commons.lang.math.LongRange;
 import org.apache.mahout.math.Vector;
 import org.apache.reef.tang.annotations.Parameter;
 
 import javax.inject.Inject;
 import java.util.Map;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class LogisticRegMainCmpTask extends UserComputeTask
     implements DataReduceSender<LogisticRegSummary>, DataBroadcastReceiver<LinearModel> {
 
+  private static final Logger LOG = Logger.getLogger(LogisticRegMainCmpTask.class.getName());
 
   private double stepSize;
   private final Loss loss;
   private final Regularization regularization;
   private final String dataType;
+  private final WorkloadQuota workloadQuota;
   private final MemoryStore memoryStore;
   private LinearModel model;
   private int posNum = 0;
@@ -50,11 +57,13 @@ public class LogisticRegMainCmpTask extends UserComputeTask
                                 final Loss loss,
                                 final Regularization regularization,
                                 @Parameter(RowDataType.class) final String dataType,
+                                final WorkloadQuota workloadQuota,
                                 final MemoryStore memoryStore) {
     this.stepSize = stepSize;
     this.loss = loss;
     this.regularization = regularization;
     this.dataType = dataType;
+    this.workloadQuota = workloadQuota;
     this.memoryStore = memoryStore;
   }
 
@@ -64,23 +73,34 @@ public class LogisticRegMainCmpTask extends UserComputeTask
     // measure accuracy
     posNum = 0;
     negNum = 0;
-    final Map<?, Row> rows = memoryStore.getElasticStore().getAll(dataType);
-    for (final Row row : rows.values()) {
-      final double output = row.getOutput();
-      final double predict = model.predict(row.getFeature());
-      if (output * predict > 0) {
-        posNum++;
-      } else {
-        negNum++;
-      }
+
+    final Set<LongRange> ranges = workloadQuota.get(dataType);
+
+    if (ranges == null) {
+      LOG.log(Level.INFO, "No {0} type data assigned to this task", dataType);
+      return;
     }
 
-    // optimize
-    for (final Row row : rows.values()) {
-      final double output = row.getOutput();
-      final Vector input = row.getFeature();
-      final Vector gradient = loss.gradient(input, model.predict(input), output).plus(regularization.gradient(model));
-      model.setParameters(model.getParameters().minus(gradient.times(stepSize)));
+    for (final LongRange range : ranges) {
+      final Map<?, Row> rows = memoryStore.getElasticStore().getRange(dataType,
+          range.getMinimumLong(), range.getMaximumLong());
+      for (final Row row : rows.values()) {
+        final double output = row.getOutput();
+        final double predict = model.predict(row.getFeature());
+        if (output * predict > 0) {
+          posNum++;
+        } else {
+          negNum++;
+        }
+      }
+
+      // optimize
+      for (final Row row : rows.values()) {
+        final double output = row.getOutput();
+        final Vector input = row.getFeature();
+        final Vector gradient = loss.gradient(input, model.predict(input), output).plus(regularization.gradient(model));
+        model.setParameters(model.getParameters().minus(gradient.times(stepSize)));
+      }
     }
   }
 
