3d264ca289164c20f61019f90cf93417c082c21d
WooYeon <wylee.xyzi@gmail.com>
2016-01-06 19:03:37 +0900

    Expose get/put interface methods of SubMemoryStore upto MemoryStore

--- services/elastic-memory/src/main/java/edu/snu/cay/services/em/evaluator/api/MemoryStore.java
+++ services/elastic-memory/src/main/java/edu/snu/cay/services/em/evaluator/api/MemoryStore.java
@@ -16,6 +16,11 @@
 package edu.snu.cay.services.em.evaluator.api;
 
 import org.apache.reef.annotations.audience.EvaluatorSide;
+import org.apache.reef.io.network.util.Pair;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * Evaluator-side interface of MemoryStore, which provides key-value style SubMemoryStore,
@@ -29,4 +34,109 @@ public interface MemoryStore {
    * may be migrated around evaluators for job optimization.
    */
   SubMemoryStore getElasticStore();
+
+    /**
+   * Register a data item of a certain data type to this store.
+   *
+   * @param dataType string that represents a certain data type
+   * @param id global unique identifier of item
+   * @param value data item to register
+   * @param <T> actual data type
+   */
+  <T> void put(String dataType, long id, T value);
+
+  /**
+   * Register data items of a certain data type to this store.
+   *
+   * @param dataType string that represents a certain data type
+   * @param ids list of global unique identifiers for each item
+   * @param values list of data items to register
+   * @param <T> actual data type
+   */
+  <T> void putList(String dataType, List<Long> ids, List<T> values);
+
+  /**
+   * Fetch a certain data item from this store.
+   *
+   * @param dataType string that represents a certain data type
+   * @param id global unique identifier of item
+   * @param <T> actual data type
+   * @return a {@link Pair} of the data id and the actual data item,
+   *         or {@code null} if no item is associated with the given id
+   */
+  <T> Pair<Long, T> get(String dataType, long id);
+
+  /**
+   * Fetch all data of a certain data type from this store.
+   * The returned list is a shallow copy of the internal data structure.
+   *
+   * @param dataType string that represents a certain data type
+   * @param <T> actual data type
+   * @return a map of data ids and the corresponding data items.
+   *         This map may be empty if no items of {@code dataType} are present.
+   */
+  <T> Map<Long, T> getAll(String dataType);
+
+  /**
+   * Fetch data items from this store whose ids are between {@code startId} and {@code endId}, both inclusive.
+   * The returned list is a shallow copy of the internal data structure.
+   *
+   * @param dataType string that represents a certain data type
+   * @param startId minimum value of the ids of items to fetch
+   * @param endId maximum value of the ids of items to fetch
+   * @param <T> actual data type
+   * @return a map of data ids and the corresponding data items.
+   *         This map may be empty if no items meet the given conditions.
+   */
+  <T> Map<Long, T> getRange(String dataType, long startId, long endId);
+
+  /**
+   * Fetch and remove a certain data item from this store.
+   *
+   * @param dataType string that represents a certain data type
+   * @param id global unique identifier of item
+   * @param <T> actual data type
+   * @return a {@link Pair} of the data id and the actual data item,
+   *         or {@code null} if no item is associated with the given id
+   */
+  <T> Pair<Long, T> remove(String dataType, long id);
+
+  /**
+   * Fetch and remove all data of a certain data type from this store.
+   * A {@code getAll(dataType)} after this method will return null.
+   *
+   * @param dataType string that represents a certain data type
+   * @param <T> actual data type
+   * @return a map of data ids and the corresponding data items.
+   *         This map may be empty if no items of {@code dataType} are present.
+   */
+  <T> Map<Long, T> removeAll(String dataType);
+
+  /**
+   * Fetch and remove data items from this store
+   * whose ids are between {@code startId} and {@code endId}, both inclusive.
+   *
+   * @param dataType string that represents a certain data type
+   * @param startId minimum value of the ids of items to fetch
+   * @param endId maximum value of the ids of items to fetch
+   * @param <T> actual data type
+   * @return a map of data ids and the corresponding data items.
+   *         This map may be empty if no items meet the given conditions.
+   */
+  <T> Map<Long, T> removeRange(String dataType, long startId, long endId);
+
+  /**
+   * Fetch all data types of items residing in this store, without duplicates.
+   *
+   * @return a set of strings that indicate the data types in this store
+   */
+  Set<String> getDataTypes();
+
+  /**
+   * Fetch the number of items associated with a certain data type.
+   *
+   * @param dataType string that represents a certain data type
+   * @return number of items associated with {@code dataType}
+   */
+  int getNumUnits(String dataType);
 }
