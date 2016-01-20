ed368af44f8e3337825f5c21178fea63e414b4d6
Brian (Sunghoon) Cho <chobrian@gmail.com>
2016-01-20 14:42:17 +0900

    Add Partitioned (single-node) parameter server.

---
+++ services/ps/src/main/java/edu/snu/cay/services/ps/driver/impl/PartitionedParameterServerManager.java
@@ -0,0 +1,97 @@
+/*
+ * Copyright (C) 2016 Seoul National University
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package edu.snu.cay.services.ps.driver.impl;
+
+import edu.snu.cay.services.ps.driver.api.ParameterServerManager;
+import edu.snu.cay.services.ps.ns.EndpointId;
+import edu.snu.cay.services.ps.ns.PSMessageHandler;
+import edu.snu.cay.services.ps.server.partitioned.PartitionedParameterServer;
+import edu.snu.cay.services.ps.server.partitioned.PartitionedServerSideMsgHandler;
+import edu.snu.cay.services.ps.server.partitioned.PartitionedServerSideReplySender;
+import edu.snu.cay.services.ps.server.partitioned.PartitionedServerSideReplySenderImpl;
+import edu.snu.cay.services.ps.server.partitioned.parameters.NumPartitions;
+import edu.snu.cay.services.ps.server.partitioned.parameters.QueueSize;
+import edu.snu.cay.services.ps.worker.api.ParameterWorker;
+import edu.snu.cay.services.ps.worker.impl.SingleNodeParameterWorker;
+import org.apache.reef.annotations.audience.DriverSide;
+import org.apache.reef.driver.context.ServiceConfiguration;
+import org.apache.reef.tang.Configuration;
+import org.apache.reef.tang.Tang;
+import org.apache.reef.tang.annotations.Parameter;
+
+import javax.inject.Inject;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * Manager class for a Partitioned Parameter Server, that supports atomic,
+ * in-order processing of push and pull operations, running on a single Evaluator.
+ * Partitions are based on the hash of the key.
+ * Each partition consists of a queue, kvStore, and thread.
+ *
+ * This manager does NOT handle server or worker faults.
+ */
+@DriverSide
+public final class PartitionedParameterServerManager implements ParameterServerManager {
+  private static final String SERVER_ID = "SINGLE_NODE_SERVER_ID";
+  private static final String WORKER_ID_PREFIX = "SINGLE_NODE_WORKER_ID_";
+
+  private final int numPartitions;
+  private final int queueSize;
+  private final AtomicInteger numWorkers;
+
+  @Inject
+  private PartitionedParameterServerManager(@Parameter(NumPartitions.class) final int numPartitions,
+                                            @Parameter(QueueSize.class) final int queueSize) {
+    this.numPartitions = numPartitions;
+    this.queueSize = queueSize;
+    this.numWorkers = new AtomicInteger(0);
+  }
+
+  /**
+   * Returns worker-side service configuration.
+   * Sets {@link SingleNodeParameterWorker} as the {@link ParameterWorker} class.
+   */
+  @Override
+  public Configuration getWorkerServiceConfiguration() {
+    final int workerIndex = numWorkers.getAndIncrement();
+
+    return Tang.Factory.getTang()
+        .newConfigurationBuilder(ServiceConfiguration.CONF
+            .set(ServiceConfiguration.SERVICES, SingleNodeParameterWorker.class)
+            .build())
+        .bindImplementation(ParameterWorker.class, SingleNodeParameterWorker.class)
+        .bindNamedParameter(ServerId.class, SERVER_ID)
+        .bindNamedParameter(EndpointId.class, WORKER_ID_PREFIX + workerIndex)
+        .build();
+  }
+
+  /**
+   * Returns server-side service configuration.
+   */
+  @Override
+  public Configuration getServerServiceConfiguration() {
+    return Tang.Factory.getTang()
+        .newConfigurationBuilder(ServiceConfiguration.CONF
+            .set(ServiceConfiguration.SERVICES, PartitionedParameterServer.class)
+            .build())
+        .bindNamedParameter(EndpointId.class, SERVER_ID)
+        .bindNamedParameter(PSMessageHandler.class, PartitionedServerSideMsgHandler.class)
+        .bindNamedParameter(NumPartitions.class, Integer.toString(numPartitions))
+        .bindNamedParameter(QueueSize.class, Integer.toString(queueSize))
+        .bindImplementation(PartitionedServerSideReplySender.class, PartitionedServerSideReplySenderImpl.class)
+        .build();
+  }
+}
---
+++ services/ps/src/main/java/edu/snu/cay/services/ps/server/partitioned/PartitionedParameterServer.java
@@ -0,0 +1,312 @@
+/*
+ * Copyright (C) 2016 Seoul National University
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package edu.snu.cay.services.ps.server.partitioned;
+
+import edu.snu.cay.services.ps.server.api.ParameterUpdater;
+import edu.snu.cay.services.ps.server.partitioned.parameters.NumPartitions;
+import edu.snu.cay.services.ps.server.partitioned.parameters.QueueSize;
+import org.apache.reef.annotations.audience.EvaluatorSide;
+import org.apache.reef.tang.annotations.Parameter;
+
+import javax.inject.Inject;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * A Partitioned Parameter Server.
+ * Receives push and pull operations from (e.g., from the network) and immediately queues them.
+ * The processing loop (for each partition) applies these operations in order; for pull operations
+ * this results in a send call via {@link PartitionedServerSideReplySender}.
+ * For more information about the partition implementation, see {@link Partition}.
+ *
+ * Supports a static number of partitions (the number of partitions is fixed at construction time).
+ */
+@EvaluatorSide
+public final class PartitionedParameterServer<K, P, V> {
+  private static final Logger LOG = Logger.getLogger(PartitionedParameterServer.class.getName());
+
+  /**
+   * Number of partitions.
+   */
+  private final int numPartitions;
+
+  /**
+   * Max size of each partition's queue.
+   */
+  private final int queueSize;
+
+  /**
+   * Thread pool, where each Partition is submitted.
+   */
+  private final ExecutorService threadPool;
+
+  /**
+   * Running partitions.
+   */
+  private final Partition<K, V>[] partitions;
+
+  /**
+   * Object for processing preValues and applying updates to existing values.
+   */
+  private final ParameterUpdater<K, P, V> parameterUpdater;
+
+  /**
+   * Sender that sends pull responses.
+   */
+  private final PartitionedServerSideReplySender sender;
+
+  @Inject
+  private PartitionedParameterServer(@Parameter(NumPartitions.class) final int numPartitions,
+                                     @Parameter(QueueSize.class) final int queueSize,
+                                     final ParameterUpdater<K, P, V> parameterUpdater,
+                                     final PartitionedServerSideReplySender sender) {
+    this.numPartitions = numPartitions;
+    this.queueSize = queueSize;
+    this.threadPool = Executors.newFixedThreadPool(numPartitions);
+    this.partitions = initPartitions();
+    this.parameterUpdater = parameterUpdater;
+    this.sender = sender;
+  }
+
+  /**
+   * Call after initializing numPartitions and threadPool.
+   */
+  @SuppressWarnings("unchecked")
+  private Partition<K, V>[] initPartitions() {
+    LOG.log(Level.INFO, "Initializing {0} partitions", numPartitions);
+    final Partition<K, V>[] initialized = new Partition[numPartitions];
+    for (int i = 0; i < numPartitions; i++) {
+      initialized[i] = new Partition<>(queueSize);
+      threadPool.submit(initialized[i]);
+    }
+    return initialized;
+  }
+
+  /**
+   * Process a {@code preValue} sent from a worker and store the resulting value.
+   * Uses {@link ParameterUpdater} to generate a value from {@code preValue} and to apply the generated value to
+   * the k-v store.
+   *
+   * The push operation is enqueued to its partition and returned immediately.
+   *
+   * @param key key object that {@code preValue} is associated with
+   * @param preValue preValue sent from the worker
+   */
+  public void push(final K key, final P preValue) {
+    partitions[getPartitionIndex(key)].enqueue(new PushOp(key, preValue));
+  }
+
+  /**
+   * Reply to srcId via {@link PartitionedServerSideReplySender}
+   * with the value corresponding to the key.
+   *
+   * The pull operation is enqueued to its partition and returned immediately.
+   *
+   * @param key key object that the requested {@code value} is associated with
+   * @param srcId network Id of the requester
+   */
+  public void pull(final K key, final String srcId) {
+    partitions[getPartitionIndex(key)].enqueue(new PullOp(key, srcId));
+  }
+
+  private int getPartitionIndex(final K key) {
+    return key.hashCode() % numPartitions;
+  }
+
+  /**
+   * @return number of operations pending, on all queues
+   */
+  public int opsPending() {
+    int sum = 0;
+    for (int i = 0; i < numPartitions; i++) {
+      sum += partitions[i].opsPending();
+    }
+    return sum;
+  }
+
+  /**
+   * A generic operation; operations are queued at each Partition.
+   */
+  private interface Op<K, V> {
+    /**
+     * Method to apply when dequeued by the Partition.
+     * @param kvStore the raw kvStore map, provided by the Partition.
+     */
+    void apply(Map<K, V> kvStore);
+  }
+
+  /**
+   * A push operation.
+   */
+  private class PushOp implements Op<K, V> {
+    private final K key;
+    private final P preValue;
+
+    public PushOp(final K key, final P preValue) {
+      this.key = key;
+      this.preValue = preValue;
+    }
+
+    /**
+     * Read from kvStore, modify (update), and write to kvStore.
+     */
+    @Override
+    public void apply(final Map<K, V> kvStore) {
+      if (!kvStore.containsKey(key)) {
+        kvStore.put(key, parameterUpdater.initValue(key));
+      }
+
+      final V deltaValue = parameterUpdater.process(key, preValue);
+      if (deltaValue == null) {
+        return;
+      }
+
+      final V updatedValue = parameterUpdater.update(kvStore.get(key), deltaValue);
+      kvStore.put(key, updatedValue);
+    }
+  }
+
+  /**
+   * A pull operation.
+   */
+  private class PullOp implements Op<K, V> {
+    private final K key;
+    private final String srcId;
+
+    public PullOp(final K key, final String srcId) {
+      this.key = key;
+      this.srcId = srcId;
+    }
+
+    /**
+     * Read from kvStore and send the key-value pair to srcId.
+     * To ensure atomicity, the key-value pair should be serialized immediately in sender.
+     */
+    @Override
+    public void apply(final Map<K, V> kvStore) {
+      if (!kvStore.containsKey(key)) {
+        kvStore.put(key, parameterUpdater.initValue(key));
+      }
+
+      sender.sendReplyMsg(srcId, key, kvStore.get(key));
+    }
+  }
+
+  /**
+   * A partition of the parameter server. Must be started as a thread.
+   * All push and pull operations should be sent to the appropriate partition.
+   * The partition's processing loop dequeues and applies operations to its local kvStore.
+   * The queue and single thread per partition ensures that all operations on a key
+   * are performed atomically, in order (no updates can be lost).
+   *
+   * The single queue-and-thread design provides a simple guarantee of atomicity for applying operations.
+   * It also means pull operations are queued behind push operations.
+   * This ensures that pull operations return with up-to-date information (for a single client, this
+   * is basically read-your-writes).
+   * However, it also means that pull operations may take awhile to process.
+   * Workers block for pulls, while sending pushes asynchronously.
+   * We should further explore this trade-off with real ML workloads.
+   */
+  private static class Partition<K, V> implements Runnable {
+    private static final long QUEUE_TIMEOUT_MS = 3000;
+
+    private final Map<K, V> kvStore;
+    private final BlockingQueue<Op<K, V>> queue;
+    private final ArrayList<Op<K, V>> localOps; // Operations drained from the queue, and processed locally.
+    private final int drainSize; // Max number of operations to drain per iteration.
+
+    private volatile boolean shutdown = false;
+
+    public Partition(final int queueSize) {
+      this.kvStore = new HashMap<>();
+      this.queue = new ArrayBlockingQueue<>(queueSize);
+      this.drainSize = queueSize / 10;
+      this.localOps = new ArrayList<>(drainSize);
+    }
+
+    /**
+     * Enqueue an operation onto the queue, blocking if the queue is full.
+     * When the queue is full, this method will block; thus, a full queue will block the thread calling
+     * enqueue, e.g., from the NCS message thread pool, until the queue is drained. This seems reasonable,
+     * as it will block client messages from being processed and overloading the system.
+     *
+     * An alternative would be to send a "busy" message in response, and have
+     * the client resend the operation. This will require changes in the client as well.
+     *
+     * @param op the operation to enqueue
+     */
+    public void enqueue(final Op<K, V> op) {
+      try {
+        queue.put(op);
+      } catch (final InterruptedException e) {
+        LOG.log(Level.SEVERE, "Enqueue failed with InterruptedException", e);
+        return;
+      }
+    }
+
+    /**
+     * @return number of pending operations in the queue.
+     */
+    public int opsPending() {
+      return queue.size();
+    }
+
+    /**
+     * Loop that dequeues operations and applies them.
+     * Dequeues is only performed through this thread.
+     */
+    @Override
+    public void run() {
+      while (!shutdown) {
+        // First, poll and apply. The timeout allows the run thread to shutdown cleanly within timeout ms.
+        try {
+          final Op<K, V> op = queue.poll(QUEUE_TIMEOUT_MS, TimeUnit.MILLISECONDS);
+          if (op == null) {
+            continue;
+          }
+          op.apply(kvStore);
+        } catch (final InterruptedException e) {
+          LOG.log(Level.SEVERE, "Poll failed with InterruptedException", e);
+          continue;
+        }
+
+        // Then, drain up to LOCAL_OPS_SIZE of the remaining queue and apply.
+        // Calling drainTo does not block if queue is empty, which is why we poll first.
+        // This should be faster than polling each op, because the blocking queue's lock is only acquired once.
+        queue.drainTo(localOps, drainSize);
+        for (final Op<K, V> op : localOps) {
+          op.apply(kvStore);
+        }
+        localOps.clear();
+      }
+    }
+
+    /**
+     * Cleanly shutdown the run thread.
+     */
+    public void shutdown() {
+      shutdown = true;
+    }
+  }
+}
