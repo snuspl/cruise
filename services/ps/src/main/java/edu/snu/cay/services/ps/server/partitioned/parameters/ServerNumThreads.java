d52e920eccb7d06d478bce141edb72b63025fcfd
Jason (Joo Seong) Jeong <culex496782@gmail.com>
2016-04-22 10:38:13 +0900

    align comments for readability

--- services/ps/src/main/java/edu/snu/cay/services/ps/server/partitioned/parameters/ServerNumThreads.java
+++ services/ps/src/main/java/edu/snu/cay/services/ps/server/partitioned/parameters/ServerNumThreads.java
@@ -18,7 +18,8 @@ package edu.snu.cay.services.ps.server.partitioned.parameters;
 import org.apache.reef.tang.annotations.Name;
 import org.apache.reef.tang.annotations.NamedParameter;
 
-@NamedParameter(doc = "Number of the threads that run operations in each server", default_value = "2",
-    short_name = "serverNumThreads")
+@NamedParameter(doc = "Number of the threads that run operations in each server",
+                default_value = "2",
+                short_name = "serverNumThreads")
 public final class ServerNumThreads implements Name<Integer> {
 }
