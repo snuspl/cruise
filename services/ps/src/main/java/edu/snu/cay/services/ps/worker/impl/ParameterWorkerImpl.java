5e1e7f8469959f3ed33113728dc9a97bd9b5ba74
WooYeon <wylee.xyzi@gmail.com>
2016-06-13 21:46:33 +0900

    Make ParameterWorker retry when catching NetworkException

--- services/ps/src/main/java/edu/snu/cay/services/ps/worker/impl/ParameterWorkerImpl.java
+++ services/ps/src/main/java/edu/snu/cay/services/ps/worker/impl/ParameterWorkerImpl.java
@@ -24,6 +24,7 @@ import edu.snu.cay.services.ps.worker.api.ParameterWorker;
 import edu.snu.cay.services.ps.worker.parameters.*;
 import edu.snu.cay.services.ps.common.resolver.ServerResolver;
 import org.apache.reef.annotations.audience.EvaluatorSide;
+import org.apache.reef.exception.evaluator.NetworkException;
 import org.apache.reef.io.serialization.Codec;
 import org.apache.reef.tang.InjectionFuture;
 import org.apache.reef.tang.annotations.Parameter;
@@ -266,19 +267,19 @@ public final class ParameterWorkerImpl<K, P, V> implements ParameterWorker<K, P,
    * We do not implement a true Future, because this is simpler.
    */
   private static final class PullFuture<V> {
-    private V value;
+    private V value = null;
 
     /**
-     * Block until a value is set.
+     * Block until a value is set or the maximum waiting time elapses.
+     * It returns null when it fails to get the value in given timeout.
+     * @param timeout the maximum time to wait in milliseconds
      * @return the value
      */
-    public synchronized V getValue() {
-      while (value == null) {
-        try {
-          wait();
-        } catch (final InterruptedException e) {
-          LOG.log(Level.WARNING, "InterruptedException on wait", e);
-        }
+    public synchronized V getValue(final long timeout) {
+      try {
+        wait(timeout);
+      } catch (final InterruptedException e) {
+        LOG.log(Level.WARNING, "InterruptedException on wait", e);
       }
       return value;
     }
@@ -358,7 +359,20 @@ public final class ParameterWorkerImpl<K, P, V> implements ParameterWorker<K, P,
       }
 
       // Send to remote PS
-      sender.get().sendPushMsg(serverResolver.resolveServer(encodedKey.getHash()), encodedKey, preValue);
+      while (true) {
+        // re-resolve server for every retry
+        final String serverId = serverResolver.resolveServer(encodedKey.getHash());
+        LOG.log(Level.FINEST, "Resolve server for encodedKey. key: {0}, hash: {1}",
+            new Object[]{encodedKey.getKey(), encodedKey.getHash()});
+
+        try {
+          sender.get().sendPushMsg(serverId, encodedKey, preValue);
+        } catch (final NetworkException e) {
+          LOG.log(Level.FINE, "NetworkException while sending push msg. Do retry", e);
+          continue;
+        }
+        break;
+      }
     }
   }
 
@@ -445,12 +459,31 @@ public final class ParameterWorkerImpl<K, P, V> implements ParameterWorker<K, P,
           .concurrencyLevel(1)
           .expireAfterWrite(expireTimeout, TimeUnit.MILLISECONDS)
           .build(new CacheLoader<EncodedKey<K>, Wrapped<V>>() {
+            private static final long RETRY_INTERVAL_MS = 5000;
+
             @Override
-            public Wrapped<V> load(final EncodedKey<K> encodedKey) throws Exception {
+            public Wrapped<V> load(final EncodedKey<K> encodedKey) {
               final PullFuture<V> future = new PullFuture<>();
               pendingPulls.put(encodedKey.getKey(), future);
-              sender.get().sendPullMsg(serverResolver.resolveServer(encodedKey.getHash()), encodedKey);
-              final V value = future.getValue();
+
+              V value = null;
+
+              // we assume that pull finally succeeds
+              while (value == null) {
+                // re-resolve server for every retry
+                final String serverId = serverResolver.resolveServer(encodedKey.getHash());
+                LOG.log(Level.FINEST, "Resolve server for encodedKey. key: {0}, hash: {1}",
+                    new Object[]{encodedKey.getKey(), encodedKey.getHash()});
+
+                try {
+                  sender.get().sendPullMsg(serverId, encodedKey);
+                } catch (final NetworkException e) {
+                  LOG.log(Level.FINE, "NetworkException while sending pull msg. Do retry", e);
+                  continue;
+                }
+                value = future.getValue(RETRY_INTERVAL_MS); // retry if fails
+              }
+
               pendingPulls.remove(encodedKey.getKey());
               return new Wrapped<>(value);
             }
