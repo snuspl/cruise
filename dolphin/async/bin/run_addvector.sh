d8150d1894a33d5e8413b09c64687e514da4993b
WooYeon <wylee.xyzi@gmail.com>
2016-08-09 20:14:06 +0900

    Add a run script

---
+++ dolphin/async/bin/run_addvector.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+# Copyright (C) 2016 Seoul National University
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# EXAMPLE USAGE
+# ./run_addvector.sh -split 3 -num_workers 3 -num_servers 2 -num_partitions 4 -max_iter 100 -delta 4 -start_key 100 -num_keys 100 -num_updates 2 -vector_size 5 -max_num_eval_local 5 -input run_addvector.sh -dynamic false -optimizer edu.snu.cay.services.em.optimizer.impl.EmptyPlanOptimizer -plan_executor edu.snu.cay.dolphin.async.plan.AsyncDolphinPlanExecutor -optimization_interval_ms 3000 -delay_after_optimization_ms 10000 -worker_log_period_ms 0 -server_log_period_ms 0 -server_metrics_window_ms 1000
+
+SELF_JAR=`echo ../target/dolphin-async-*-shaded.jar`
+
+LOGGING_CONFIG='-Djava.util.logging.config.class=edu.snu.cay.utils.LoggingConfig'
+
+CLASSPATH=$YARN_HOME/share/hadoop/common/*:$YARN_HOME/share/hadoop/common/lib/*:$YARN_HOME/share/hadoop/yarn/*:$YARN_HOME/share/hadoop/hdfs/*:$YARN_HOME/share/hadoop/mapreduce/lib/*:$YARN_HOME/share/hadoop/mapreduce/*
+
+YARN_CONF_DIR=$YARN_HOME/etc/hadoop
+
+ALG=edu.snu.cay.dolphin.async.examples.addvector.AddVectorREEF
+
+CMD="java -cp $YARN_CONF_DIR:$SELF_JAR:$CLASSPATH $LOGGING_CONFIG $ALG $*"
+echo $CMD
+$CMD
