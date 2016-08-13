b3cdb6459499760482f3febd5dea438c37366b96
WooYeon <wylee.xyzi@gmail.com>
2016-08-13 21:55:39 +0900

    Implement TestWatcher that enables overriding of logging level in test

---
+++ services/ps/src/test/java/edu/snu/cay/services/ps/worker/impl/TestWatcherLoggingRule.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (C) 2016 Seoul National University
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package edu.snu.cay.services.ps.worker.impl;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.junit.rules.TestWatcher;
+import org.junit.runner.Description;
+
+/**
+ * TestWatcher class to override the logging level of specific logger to the specified level.
+ * It restores the logging level after a test method is finished.
+ */
+final class TestWatcherLoggingRule extends TestWatcher {
+
+  private final String methodName;
+
+  private final Logger logger;
+
+  private final Level origLoggingLevel;
+
+  private final Level targetLoggingLevel;
+
+  TestWatcherLoggingRule(final String methodName, final String loggerName,
+                                final Level targetLoggingLevel) {
+    this.methodName = methodName;
+    this.logger = Logger.getLogger(loggerName);
+    this.origLoggingLevel = logger.getLevel();
+    this.targetLoggingLevel = targetLoggingLevel;
+  }
+
+  @Override
+  public void starting(final Description desc) {
+    if (desc.getMethodName().equals(methodName)) {
+      logger.setLevel(targetLoggingLevel);
+    }
+  }
+
+  @Override
+  public void finished(final Description desc) {
+    logger.setLevel(origLoggingLevel);
+  }
+}
