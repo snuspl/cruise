f1e631871b881aa922c4bb26b42f6b010cfff8da
Soojeong Kim <epik03sj@gmail.com>
2016-10-31 15:02:17 +0900

    fix minor bug

--- dolphin/async/src/main/java/edu/snu/cay/dolphin/async/dnn/data/NeuralNetworkDataParser.java
+++ dolphin/async/src/main/java/edu/snu/cay/dolphin/async/dnn/data/NeuralNetworkDataParser.java
@@ -36,8 +36,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  * Data parser for neural network.
