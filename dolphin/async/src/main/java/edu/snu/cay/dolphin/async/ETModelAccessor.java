4e1b39b556de8b9b6306119c47d1948a861fb499
Yunseong Lee <yunseong@apache.org>
2017-04-22 11:56:53 +0900

    Fix pull(list)

--- dolphin/async/src/main/java/edu/snu/cay/dolphin/async/ETModelAccessor.java
+++ dolphin/async/src/main/java/edu/snu/cay/dolphin/async/ETModelAccessor.java
@@ -61,7 +61,7 @@ public final class ETModelAccessor<K, P, V> implements ModelAccessor<K, P, V> {
   public List<V> pull(final List<K> keys) {
     final List<Future<V>> resultList = new ArrayList<>(keys.size());
 
-    keys.forEach(key -> resultList.add(modelTable.get(key)));
+    keys.forEach(key -> resultList.add(modelTable.getOrInit(key)));
 
     final List<V> resultValues = new ArrayList<>(keys.size());
     for (final Future<V> opResult : resultList) {
