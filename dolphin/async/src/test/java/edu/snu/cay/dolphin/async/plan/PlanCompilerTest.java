0bcc70fb9db98c8eb7366518f71bd0704e5a32ad
WooYeon <wylee.xyzi@gmail.com>
2017-04-28 01:02:26 +0900

    Resolve YS's comments

--- dolphin/async/src/test/java/edu/snu/cay/dolphin/async/plan/PlanCompilerTest.java
+++ dolphin/async/src/test/java/edu/snu/cay/dolphin/async/plan/PlanCompilerTest.java
@@ -17,6 +17,7 @@ package edu.snu.cay.dolphin.async.plan;
 
 import edu.snu.cay.dolphin.async.ETDolphinDriver;
 import edu.snu.cay.dolphin.async.optimizer.parameters.Constants;
+import edu.snu.cay.services.em.optimizer.api.DataInfo;
 import edu.snu.cay.services.em.optimizer.impl.DataInfoImpl;
 import edu.snu.cay.services.em.plan.api.Plan;
 import edu.snu.cay.services.em.plan.impl.TransferStepImpl;
@@ -43,6 +44,7 @@ import static org.junit.Assert.*;
 @PrepareForTest({ETDolphinDriver.class})
 public class PlanCompilerTest {
   private static final String EVAL_ID_PREFIX = "EVAL-";
+  private static final DataInfo DUMMY_DATA_INFO = new DataInfoImpl();
 
   private PlanCompiler compiler;
 
@@ -61,16 +63,17 @@ public class PlanCompilerTest {
   public void testMovePlan() {
     final Plan plan = PlanImpl.newBuilder()
         .addTransferStep(Constants.NAMESPACE_WORKER,
-            new TransferStepImpl(EVAL_ID_PREFIX + 0, EVAL_ID_PREFIX + 1, new DataInfoImpl()))
+            new TransferStepImpl(EVAL_ID_PREFIX + 0, EVAL_ID_PREFIX + 1, DUMMY_DATA_INFO))
         .addTransferStep(Constants.NAMESPACE_WORKER,
-            new TransferStepImpl(EVAL_ID_PREFIX + 2, EVAL_ID_PREFIX + 3, new DataInfoImpl()))
+            new TransferStepImpl(EVAL_ID_PREFIX + 2, EVAL_ID_PREFIX + 3, DUMMY_DATA_INFO))
         .addTransferStep(Constants.NAMESPACE_SERVER,
-            new TransferStepImpl(EVAL_ID_PREFIX + 4, EVAL_ID_PREFIX + 5, new DataInfoImpl()))
+            new TransferStepImpl(EVAL_ID_PREFIX + 4, EVAL_ID_PREFIX + 5, DUMMY_DATA_INFO))
         .addTransferStep(Constants.NAMESPACE_SERVER,
-            new TransferStepImpl(EVAL_ID_PREFIX + 6, EVAL_ID_PREFIX + 7, new DataInfoImpl()))
+            new TransferStepImpl(EVAL_ID_PREFIX + 6, EVAL_ID_PREFIX + 7, DUMMY_DATA_INFO))
         .build();
 
-    final ETPlan etPlan = compiler.compile(plan, 0);
+    final int numAvailableEvals = 0; // no meaning because this plan has no change about resource (executor)
+    final ETPlan etPlan = compiler.compile(plan, numAvailableEvals);
 
     // moves have no dependency
     final Set<Op> initialOps = etPlan.getInitialOps();
@@ -87,10 +90,11 @@ public class PlanCompilerTest {
     final Plan plan = PlanImpl.newBuilder()
         .addEvaluatorsToAdd(Constants.NAMESPACE_WORKER, Collections.singletonList(workerId))
         .addTransferStep(Constants.NAMESPACE_WORKER,
-            new TransferStepImpl(EVAL_ID_PREFIX + 1, workerId, new DataInfoImpl()))
+            new TransferStepImpl(EVAL_ID_PREFIX + 1, workerId, DUMMY_DATA_INFO))
         .build();
 
-    final ETPlan etPlan = compiler.compile(plan, 1);
+    final int numAvailableEvals = 1; // should larger than or equal to 1 for adding a new worker
+    final ETPlan etPlan = compiler.compile(plan, numAvailableEvals);
 
     // Adding one worker involves: allocate, associate, move, subscribe, start
     assertEquals(5, etPlan.getNumTotalOps());
@@ -109,9 +113,10 @@ public class PlanCompilerTest {
     final Op secondOp0 = iter.next();
     final Op secondOp1 = iter.next();
 
+    // one operation should be SUBSCRIBE and the other should be ASSOCIATE
     assertTrue(secondOp0.getOpType().equals(Op.OpType.SUBSCRIBE) || secondOp0.getOpType().equals(Op.OpType.ASSOCIATE));
     assertTrue(secondOp1.getOpType().equals(Op.OpType.SUBSCRIBE) || secondOp1.getOpType().equals(Op.OpType.ASSOCIATE));
-    assertFalse(secondOp0.getOpType().equals(secondOp1.getOpType()));
+    assertNotEquals(secondOp0.getOpType(), secondOp1.getOpType());
 
     final Op subscribeOp = secondOp0.getOpType().equals(Op.OpType.SUBSCRIBE) ? secondOp0 : secondOp1;
     final Op associateOp = secondOp0.getOpType().equals(Op.OpType.ASSOCIATE) ? secondOp0 : secondOp1;
@@ -139,10 +144,11 @@ public class PlanCompilerTest {
     final Plan plan = PlanImpl.newBuilder()
         .addEvaluatorsToAdd(Constants.NAMESPACE_SERVER, Collections.singletonList(serverId))
         .addTransferStep(Constants.NAMESPACE_SERVER,
-            new TransferStepImpl(EVAL_ID_PREFIX + 1, serverId, new DataInfoImpl()))
+            new TransferStepImpl(EVAL_ID_PREFIX + 1, serverId, DUMMY_DATA_INFO))
         .build();
 
-    final ETPlan etPlan = compiler.compile(plan, 1);
+    final int numAvailableEvals = 1; // should larger than or equal to 1 for adding a new server
+    final ETPlan etPlan = compiler.compile(plan, numAvailableEvals);
 
     // Adding one server involves: allocate, associate, move
     assertEquals(3, etPlan.getNumTotalOps());
@@ -175,10 +181,11 @@ public class PlanCompilerTest {
     final Plan plan = PlanImpl.newBuilder()
         .addEvaluatorsToDelete(Constants.NAMESPACE_WORKER, Collections.singletonList(workerId))
         .addTransferStep(Constants.NAMESPACE_WORKER,
-            new TransferStepImpl(workerId, EVAL_ID_PREFIX + 1, new DataInfoImpl()))
+            new TransferStepImpl(workerId, EVAL_ID_PREFIX + 1, DUMMY_DATA_INFO))
         .build();
 
-    final ETPlan etPlan = compiler.compile(plan, 0);
+    final int numAvailableEvals = 0; // no meaning because this plan requires no more resources
+    final ETPlan etPlan = compiler.compile(plan, numAvailableEvals);
 
     // Deleting one worker involves: stop, move, unassociate, unsubscribe, deallocate
     assertEquals(5, etPlan.getNumTotalOps());
@@ -197,11 +204,12 @@ public class PlanCompilerTest {
     final Op secondOp0 = iter.next();
     final Op secondOp1 = iter.next();
 
+    // one operation should be UNSUBSCRIBE and the other should be MOVE
     assertTrue(secondOp0.getOpType().equals(Op.OpType.UNSUBSCRIBE) ||
         secondOp0.getOpType().equals(Op.OpType.MOVE));
     assertTrue(secondOp1.getOpType().equals(Op.OpType.UNSUBSCRIBE) ||
         secondOp1.getOpType().equals(Op.OpType.MOVE));
-    assertFalse(secondOp0.getOpType().equals(secondOp1.getOpType()));
+    assertNotEquals(secondOp0.getOpType(), secondOp1.getOpType());
 
     final Op unsubscribeOp = secondOp0.getOpType().equals(Op.OpType.UNSUBSCRIBE) ? secondOp0 : secondOp1;
     final Op moveOp = secondOp0.getOpType().equals(Op.OpType.MOVE) ? secondOp0 : secondOp1;
@@ -229,10 +237,11 @@ public class PlanCompilerTest {
     final Plan plan = PlanImpl.newBuilder()
         .addEvaluatorsToDelete(Constants.NAMESPACE_SERVER, Collections.singletonList(serverId))
         .addTransferStep(Constants.NAMESPACE_SERVER,
-            new TransferStepImpl(serverId, EVAL_ID_PREFIX + 1, new DataInfoImpl()))
+            new TransferStepImpl(serverId, EVAL_ID_PREFIX + 1, DUMMY_DATA_INFO))
         .build();
 
-    final ETPlan etPlan = compiler.compile(plan, 0);
+    final int numAvailableEvals = 0; // no meaning because this plan requires no more resources
+    final ETPlan etPlan = compiler.compile(plan, numAvailableEvals);
 
     // Deleting one server involves: move, unassociate, deallocate
     assertEquals(3, etPlan.getNumTotalOps());
@@ -266,13 +275,14 @@ public class PlanCompilerTest {
     final Plan plan = PlanImpl.newBuilder()
         .addEvaluatorsToDelete(Constants.NAMESPACE_SERVER, Collections.singletonList(serverId))
         .addTransferStep(Constants.NAMESPACE_SERVER,
-            new TransferStepImpl(serverId, EVAL_ID_PREFIX + 2, new DataInfoImpl()))
+            new TransferStepImpl(serverId, EVAL_ID_PREFIX + 2, DUMMY_DATA_INFO))
         .addEvaluatorsToAdd(Constants.NAMESPACE_WORKER, Collections.singletonList(workerId))
         .addTransferStep(Constants.NAMESPACE_WORKER,
-            new TransferStepImpl(EVAL_ID_PREFIX + 2, workerId, new DataInfoImpl()))
+            new TransferStepImpl(EVAL_ID_PREFIX + 2, workerId, DUMMY_DATA_INFO))
         .build();
 
-    final ETPlan etPlan = compiler.compile(plan, 0);
+    final int numAvailableEvals = 0; // by setting is as zero, allocate should be done after deallocate
+    final ETPlan etPlan = compiler.compile(plan, numAvailableEvals);
 
     // Deleting one server involves: move, unassociate, deallocate
     // Adding one worker involves: allocate, associate, move, subscribe, start
@@ -310,9 +320,10 @@ public class PlanCompilerTest {
     final Op fifthOp0 = iter.next();
     final Op fifthOp1 = iter.next();
 
+    // one operation should be SUBSCRIBE and the other should be ASSOCIATE
     assertTrue(fifthOp0.getOpType().equals(Op.OpType.SUBSCRIBE) || fifthOp0.getOpType().equals(Op.OpType.ASSOCIATE));
     assertTrue(fifthOp1.getOpType().equals(Op.OpType.SUBSCRIBE) || fifthOp1.getOpType().equals(Op.OpType.ASSOCIATE));
-    assertFalse(fifthOp0.getOpType().equals(fifthOp1.getOpType()));
+    assertNotEquals(fifthOp0.getOpType(), fifthOp1.getOpType());
 
     final Op subscribeOp = fifthOp0.getOpType().equals(Op.OpType.SUBSCRIBE) ? fifthOp0 : fifthOp1;
     final Op associateOp = fifthOp0.getOpType().equals(Op.OpType.ASSOCIATE) ? fifthOp0 : fifthOp1;
