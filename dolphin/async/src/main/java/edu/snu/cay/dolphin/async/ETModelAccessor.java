3e746bc5c8522d366b8106b200dafa69ad84ee8d
Yunseong Lee <yunseong@apache.org>
2017-05-07 01:34:42 +0900

    Collect metrics in model accessor

--- dolphin/async/src/main/java/edu/snu/cay/dolphin/async/ETModelAccessor.java
+++ dolphin/async/src/main/java/edu/snu/cay/dolphin/async/ETModelAccessor.java
@@ -15,13 +15,16 @@
  */
 package edu.snu.cay.dolphin.async;
 
+import edu.snu.cay.dolphin.async.metric.Tracer;
 import edu.snu.cay.services.et.evaluator.api.Table;
 import edu.snu.cay.services.et.evaluator.api.TableAccessor;
 import edu.snu.cay.services.et.exceptions.TableNotExistException;
 
 import javax.inject.Inject;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 
@@ -33,6 +36,9 @@ public final class ETModelAccessor<K, P, V> implements ModelAccessor<K, P, V> {
 
   private final Table<K, V, P> modelTable;
 
+  private final Tracer pushTracer = new Tracer();
+  private final Tracer pullTracer = new Tracer();
+
   @Inject
   ETModelAccessor(final TableAccessor tableAccessor) throws TableNotExistException {
     this.modelTable = tableAccessor.getTable(MODEL_TABLE_ID);
@@ -40,27 +46,37 @@ public final class ETModelAccessor<K, P, V> implements ModelAccessor<K, P, V> {
 
   @Override
   public void push(final K key, final P deltaValue) {
+    pushTracer.startTimer();
     modelTable.updateNoReply(key, deltaValue);
+    pushTracer.recordTime(1);
   }
 
   @Override
   public V pull(final K key) {
+    pullTracer.startTimer();
+
     final Future<V> future = modelTable.getOrInit(key);
+    V result;
     while (true) {
       try {
-        return future.get();
+        result = future.get();
+        break;
       } catch (InterruptedException e) {
         // ignore and keep waiting
       } catch (ExecutionException e) {
         throw new RuntimeException(e);
       }
     }
+    
+    pullTracer.recordTime(1);
+    return result;
   }
 
   @Override
   public List<V> pull(final List<K> keys) {
-    final List<Future<V>> resultList = new ArrayList<>(keys.size());
+    pullTracer.startTimer();
 
+    final List<Future<V>> resultList = new ArrayList<>(keys.size());
     keys.forEach(key -> resultList.add(modelTable.getOrInit(key)));
 
     final List<V> resultValues = new ArrayList<>(keys.size());
@@ -80,6 +96,20 @@ public final class ETModelAccessor<K, P, V> implements ModelAccessor<K, P, V> {
       resultValues.add(result);
     }
 
+    pullTracer.recordTime(resultValues.size());
     return resultValues;
+  }
+
+  @Override
+  public synchronized Map<String, Double> getAndResetMetrics() {
+    final Map<String, Double> metrics = new HashMap<>();
+    metrics.put(METRIC_TOTAL_PULL_TIME_SEC, pullTracer.totalElapsedTime());
+    metrics.put(METRIC_AVG_PULL_TIME_SEC, pullTracer.avgTimePerElem());
+    metrics.put(METRIC_TOTAL_PUSH_TIME_SEC, pushTracer.totalElapsedTime());
+    metrics.put(METRIC_AVG_PUSH_TIME_SEC, pushTracer.avgTimePerElem());
+
+    pullTracer.resetTrace();
+    pushTracer.resetTrace();
+    return metrics;
   }
 }
