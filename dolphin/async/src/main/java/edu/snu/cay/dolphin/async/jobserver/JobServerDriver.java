f4ae459000359cd2592ef4e5346eaa798d184048
WooYeon <wylee.xyzi@gmail.com>
2017-06-18 14:30:29 +0900

    fix style error

--- dolphin/async/src/main/java/edu/snu/cay/dolphin/async/jobserver/JobServerDriver.java
+++ dolphin/async/src/main/java/edu/snu/cay/dolphin/async/jobserver/JobServerDriver.java
@@ -55,8 +55,6 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.logging.Level;
